f16914686120a92baecc4d7fbc2e0141292380e3
Chris Olah <chris@colah.ca>
2012-08-12 21:06:35 -0400

    Performance improvements in Render.hs procedure!
    After a lot of work and fun with ParallelArrays, we make our functions
    flow along the lists we're working with... This leads to performance
    improvements. However, if we do this for calculuating segs*, it hurts
    performance! Your guess is as good as mine, here... Magic?
    
    Some benchmarks:
    
    No Parallelism:
    
    (union.scad, new)
    real    0m1.691s
    user    0m1.612s
    sys     0m0.080s
    
    (union.scad, old)
    real    0m2.256s
    user    0m2.116s
    sys     0m0.140s
    
    (car_rim.scad, new)
    real    0m7.308s
    user    0m7.044s
    sys     0m0.260s
    
    (car_rim.scad, old)
    real    0m9.298s
    user    0m8.957s
    sys     0m0.340s
    
    With Parallism (ec2, +RTS -N8 -RTS):
    
    (car_rim.scad, new)
    real    0m2.572s
    user    0m12.509s
    sys     0m2.676s
    
    (car_rim.scad, old)
    real    0m3.256s
    user    0m17.113s
    sys     0m3.784s
    
    Progess, yay!!

--- Graphics/Implicit/Export/Render.hs
+++ Graphics/Implicit/Export/Render.hs
@@ -1,7 +1,11 @@
 -- Implicit CAD. Copyright (C) 2011, Christopher Olah (chris@colah.ca)
 -- Released under the GNU GPL, see LICENSE
 
+{-# LANGUAGE ParallelListComp #-}
+
 module Graphics.Implicit.Export.Render where
+
+import Debug.Trace
 
 import Graphics.Implicit.Definitions
 import Graphics.Implicit.Export.Render.Definitions
@@ -62,6 +66,10 @@ getMesh (x1, y1, z1) (x2, y2, z2) res obj =
 
 		l ! (a,b,c) = l !! c !! b !! a
 
+		pZs = [ z1 + rz*n | n <- [0.. fromIntegral nz] ]
+		pYs = [ y1 + ry*n | n <- [0.. fromIntegral ny] ]
+		pXs = [ x1 + rx*n | n <- [0.. fromIntegral nx] ]
+
 
 		{-# INLINE par3DList #-}
 		par3DList lenx leny lenz f = 
@@ -99,45 +107,100 @@ getMesh (x1, y1, z1) (x2, y2, z2) res obj =
 
 		-- Calculate segments for each side
 
-		segsZ = par3DList (nx-1) (ny-1) (nz) $ \x mx y my z mz ->
+		segsZ = {- [[[ 
+			map2  (inj3 z0) $ getSegs (x0,y0) (x1,y1) (obj **$ z0) (midA0, midA1, midB0, midB1)
+			 |x0<-pXs|x1<-tail pXs|midB0<-mX'' |midB1<-mX'T    |midA0<-mY'' |midA1<-tail mY''
+			]|y0<-pYs|y1<-tail pYs|mX'' <-mX'  |mX'T <-tail mX'|mY'' <-mY'
+			]|z0<-pZs             |mX'  <-midsX|                mY'  <-midsY 
+			] -}
+			{-let
+				iteree = zip3D3 (lag3 points) (lag3s22 midsY) (lag3s12 midsX)
+				transform (((x0,y0,z0),(x1,y1,z1)), (midA0,midA1), (midB0,midB1)) =
+					map2  (inj3 z0) $ 
+						getSegs (x0,y0) (x1,y1) (obj **$ z0) (midA0, midA1, midB0, midB1)
+				result = for3 iteree transform
+			in
+				result --`using` (parListChunk (max 1 $ div lenz 32) rdeepseq)-}
+		  par3DList (nx-1) (ny-1) (nz) $ \x mx y my z mz ->
 		       map2  (inj3 (z 0)) $ getSegs 
 		           (x 0, y 0)
                    (x 1, y 1)
                    (obj **$ z 0)
 		           (midsY ! (mx, my, mz), midsY ! (mx+1, my, mz),
-		            midsX ! (mx, my, mz), midsX ! (mx, my+1, mz))
-
-		segsY = par3DList (nx-1) (ny) (nz-1) $ \x mx y my z mz ->
+		            midsX ! (mx, my, mz), midsX ! (mx, my+1, mz)) -- -}
+		segsY = {-[[[ 
+			map2  (inj2 y0) $ getSegs (x0,z0) (x1,z1) (obj *$* y0) (midA0, midA1, midB0, midB1)
+			 |x0<-pXs|x1<-tail pXs|midB0<-mB'' |midB1<-mBT'      |midA0<-mA'' |midA1<-tail mA''
+			]|y0<-pYs|             mB'' <-mB'  |mBT' <-mBT       |mA'' <-mA'
+			]|z0<-pZs|z1<-tail pZs|mB'  <-midsX|mBT  <-tail midsX|mA'  <-midsZ 
+			]-}
+			{-let
+				iteree = zip3D3 (lag3 points) (lag3s22 midsZ) (lag3s02 midsX)
+				transform (((x0,y0,z0),(x1,y1,z1)), (midA0,midA1), (midB0,midB1)) =
+					map2  (inj2 y0) $ 
+						getSegs (x0,z0) (x1,z1) (obj *$* y0) (midA0, midA1, midB0, midB1)
+				result = for3 iteree transform
+			in
+				result -}
+			par3DList (nx-1) (ny) (nz-1) $ \x mx y my z mz ->
 		       map2  (inj2 (y 0)) $ getSegs 
 		           (x 0, z 0)
                    (x 1, z 1)
                    (obj *$* y 0)
 		           (midsZ ! (mx, my, mz), midsZ ! (mx+1, my, mz),
 		            midsX ! (mx, my, mz), midsX ! (mx, my, mz+1))
+		-- -}
 
-		segsX = par3DList (nx) (ny-1) (nz-1) $ \x mx y my z mz ->
+		segsX = 
+			{-[[[ 
+			map2  (inj1 x0) $ getSegs (y0,z0) (y1,z1) (obj $** x0) (midA0, midA1, midB0, midB1)
+			 |x0<-pXs|             midB0<-mB'' |midB1<-mBT'      |midA0<-mA'' |midA1<-mA'T
+			]|y0<-pYs|y1<-tail pYs|mB'' <-mB'  |mBT' <-mBT       |mA'' <-mA'  |mA'T <-tail mA'
+			]|z0<-pZs|z1<-tail pZs|mB'  <-midsY|mBT  <-tail midsY|mA'  <-midsZ 
+			]-}
+			{-let
+				iteree = zip3D3 (lag3 points) (lag3s12 midsZ) (lag3s02 midsY)
+				transform (((x0,y0,z0),(x1,y1,z1)), (midA0,midA1), (midB0,midB1)) =
+					map2  (inj1 x0) $ 
+						getSegs (y0,z0) (y1,z1) (obj $** x0) (midA0, midA1, midB0, midB1)
+				result = for3 iteree transform
+			in
+				result -}
+
+			par3DList (nx) (ny-1) (nz-1) $ \x mx y my z mz ->
 		       map2  (inj1 (x 0)) $ getSegs 
 		           (y 0, z 0)
                    (y 1, z 1)
                    (obj $** x 0)
 		           (midsZ ! (mx, my, mz), midsZ ! (mx, my+1, mz),
-		            midsY ! (mx, my, mz), midsY ! (mx, my, mz+1) )
+		            midsY ! (mx, my, mz), midsY ! (mx, my, mz+1) ) -- -}
 
 		-- (3) & (4) : get and tesselate loops
  
-		sqTris = [
-		        concat $ map (tesselateLoop res obj) $ getLoops $ concat [
-		                     segsX !! mz     !! my     !! mx,
-		              mapR $ segsX !! mz     !! my     !!(mx + 1),
-		              mapR $ segsY !! mz     !! my     !! mx,
-		                     segsY !! mz     !!(my + 1)!! mx,
-		                     segsZ !! mz     !! my     !! mx,
-		              mapR $ segsZ !!(mz + 1)!! my     !! mx
-		          ]
-		       | mx <- [0.. nx-1], my <- [0.. ny-1], mz <- [0.. nz-1]
+		sqTris = [[[
+		    concat $ map (tesselateLoop res obj) $ getLoops $ concat [
+		                segX''',
+		           mapR segX''T,
+		           mapR segY''',
+		                segY'T',
+		                segZ''',
+		           mapR segZT''
+		        ]
+
+			 | segZ'''<- segZ''| segZT''<- segZT'
+			 | segY'''<- segY''| segY'T'<- segY'T
+			 | segX'''<- segX''| segX''T<- tail segX''
+
+			]| segZ'' <- segZ' | segZT' <- segZT
+			 | segY'' <- segY' | segY'T <- tail segY'
+			 | segX'' <- segX'
+
+			]| segZ'  <- segsZ | segZT  <- tail segsZ
+			 | segY' <- segsY
+			 | segX' <- segsX
 		       ] `using` (parListChunk (nx*ny*(max 1 $ div nz 32)) rdeepseq)
 	
-	in mergedSquareTris $ concat sqTris -- (5) merge squares, etc
+	in mergedSquareTris $ concat $ concat $ concat sqTris -- (5) merge squares, etc
 
 
 
@@ -162,4 +225,22 @@ map2 f = map (map f)
 map2R f = map (reverse . map f)
 mapR = map reverse
 
+{-
+lagzip a = zip a (tail a)
+tupzip (a,b) = zip a b
+tupzip3 (a,b,c) = zip3 a b c
 
+zipD2 a b = map tupzip $ zip a b
+zipD3 a b = map (map tupzip) . map tupzip $ zip a b
+
+zip3D3 a b c = map (map tupzip3) . map tupzip3 $ zip3 a b c
+
+lag3s02 = map (map tupzip) . map tupzip . lagzip
+lag3s12 = map (map tupzip) . map lagzip
+lag3s22 = map (map lagzip)
+
+lag3 :: [[[a]]] -> [[[(a,a)]]]
+lag3 a = zipD3 a $ map (map tail) $ map tail $ tail a
+
+for3 = flip (map . map . map)
+-}
