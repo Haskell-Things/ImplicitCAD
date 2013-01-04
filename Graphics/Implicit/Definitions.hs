2b4fa849a4d912b792a63ad64dc136085d5a902a
Christopher Olah <chris@colah.ca>
2013-01-04 14:53:23 -0500

    Fix the overlapping Show (a -> b) instance bug.

--- Graphics/Implicit/Definitions.hs
+++ Graphics/Implicit/Definitions.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, OverlappingInstances #-}
 
 -- Implicit CAD. Copyright (C) 2011, Christopher Olah (chris@colah.ca)
 -- Released under the GNU GPL, see LICENSE
