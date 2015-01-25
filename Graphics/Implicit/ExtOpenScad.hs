aa41e5b94b186299ee61333844f184f6ed624fad
Julia Longtin <julialongtin@diasp.org>
2015-01-25 02:27:57 +0000

    Remove unnecessary variable declaration.

--- Graphics/Implicit/ExtOpenScad.hs
+++ Graphics/Implicit/ExtOpenScad.hs
@@ -24,7 +24,7 @@ runOpenscad s =
 	let
 		initial =  defaultObjects
 		rearrange (_, (varlookup, ovals, _ , _ , _)) = (varlookup, obj2s, obj3s) where
-			(obj2s, obj3s, others) = divideObjs ovals
+			(obj2s, obj3s, _ ) = divideObjs ovals
 	in case parseProgram "" s of
 		Left e -> Left e
 		Right sts -> Right
