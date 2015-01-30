374c33040a5df6f779aa3d5811dcb58d8e95f7c9
Julia Longtin <julialongtin@diasp.org>
2015-01-30 07:17:31 +0000

    Fix apparent typo in the pack implementations.

--- Graphics/Implicit/Primitives.hs
+++ Graphics/Implicit/Primitives.hs
@@ -179,7 +179,7 @@ pack3 (dx, dy) sep objs =
 		boxDropZ ((a,b,c),(d,e,f)) = ((a,b),(d,e))
 		withBoxes :: [(Box2, SymbolicObj3)]
 		withBoxes = map (\obj -> ( boxDropZ $ getBox3 obj, obj)) objs
-	in case pack ((0,0),(dy,dy)) sep withBoxes of
+	in case pack ((0,0),(dx,dy)) sep withBoxes of
 			(a, []) -> Just $ union $ map (\((x,y),obj) -> translate (x,y,0) obj) a
 			_ -> Nothing
 				
@@ -194,7 +194,7 @@ pack2 (dx, dy) sep objs =
 	let
 		withBoxes :: [(Box2, SymbolicObj2)]
 		withBoxes = map (\obj -> ( getBox2 obj, obj)) objs
-	in case pack ((0,0),(dy,dy)) sep withBoxes of
+	in case pack ((0,0),(dx,dy)) sep withBoxes of
 			(a, []) -> Just $ union $ map (\((x,y),obj) -> translate (x,y) obj) a
 			_ -> Nothing
 
