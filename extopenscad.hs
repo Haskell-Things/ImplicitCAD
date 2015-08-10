b49d256c1a3942d454caa0ec0eb3a21799350e0b
Mike MacHenry <mike.machenry@gmail.com>
2015-08-10 13:44:56 -0400

    Remove most warnings and hlint warnings in main executable.

--- extopenscad.hs
+++ extopenscad.hs
@@ -6,18 +6,14 @@
 -- Let's make it convenient to run our extended openscad format code
 
 -- Let's be explicit about what we're getting from where :)
-import System.IO (openFile, IOMode (ReadMode), hGetContents, hClose)
-import Graphics.Implicit (runOpenscad, writeSVG, writeBinSTL, writeOBJ, writeSCAD3, writeSCAD2, writeGCodeHacklabLaser, writeTHREEJS, writePNG2, writePNG3)
+import Graphics.Implicit (runOpenscad, writeSVG, writeBinSTL, writeOBJ, writeSCAD3, writeSCAD2, writeGCodeHacklabLaser, writePNG2, writePNG3)
 import Graphics.Implicit.ExtOpenScad.Definitions (OVal (ONum))
 import Graphics.Implicit.ObjectUtil (getBox2, getBox3)
-import Graphics.Implicit.Definitions (xmlErrorOn, errorMessage, SymbolicObj2, SymbolicObj3)
+import Graphics.Implicit.Definitions (xmlErrorOn, SymbolicObj2, SymbolicObj3)
 import qualified Data.Map as Map hiding (null)
 import Data.Maybe as Maybe
 import Data.Char
-import Data.Monoid (Monoid, mappend)
 import Data.Tuple (swap)
-import Text.ParserCombinators.Parsec (errorPos, sourceLine)
-import Text.ParserCombinators.Parsec.Error
 import Data.IORef (writeIORef)
 import Data.AffineSpace
 import Control.Applicative
@@ -158,7 +154,7 @@ main = do
     case runOpenscad content of
         Left err -> print err
         Right openscadProgram -> do
-            s@(vars, obj2s, obj3s) <- openscadProgram
+            s@(_vars, obj2s, obj3s) <- openscadProgram
             let res = Maybe.fromMaybe (getRes s) (resolution args)
             let basename = fst (splitExtension $ inputFile args)
             let posDefExt = case format of
