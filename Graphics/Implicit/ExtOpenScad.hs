0eece6a75614b7cf483606286cc8f8212c47597a
Julia Longtin <julialongtin@diasp.org>
2015-11-05 14:05:55 +0000

    Be more explicit about what we depend on, correct the english in a comment, and change some indention.

--- Graphics/Implicit/ExtOpenScad.hs
+++ Graphics/Implicit/ExtOpenScad.hs
@@ -1,39 +1,36 @@
 -- Implicit CAD. Copyright (C) 2011, Christopher Olah (chris@colah.ca)
+-- Copyright (C) 2014 2015, Julia Longtin (julial@turinglace.com)
 -- Released under the GNU GPL, see LICENSE
 
 -- We'd like to parse openscad code, with some improvements, for backwards compatability.
 
 module Graphics.Implicit.ExtOpenScad (runOpenscad, OVal (..) ) where
 
-
 import Graphics.Implicit.Definitions (SymbolicObj2, SymbolicObj3)
-import Graphics.Implicit.ExtOpenScad.Definitions
-import Graphics.Implicit.ExtOpenScad.Parser.Statement
-import Graphics.Implicit.ExtOpenScad.Eval.Statement
+import Graphics.Implicit.ExtOpenScad.Definitions (VarLookup, OVal(..))
+import Graphics.Implicit.ExtOpenScad.Parser.Statement (parseProgram)
+import Graphics.Implicit.ExtOpenScad.Eval.Statement (runStatementI)
 import Graphics.Implicit.ExtOpenScad.Default (defaultObjects)
-import Graphics.Implicit.ExtOpenScad.Util.OVal
+import Graphics.Implicit.ExtOpenScad.Util.OVal (divideObjs)
 
 import qualified Text.Parsec.Error as Parsec (ParseError)
-import qualified Control.Monad as Monad
-import qualified Control.Monad.State as State
-import qualified System.Directory as Dir
+import qualified Control.Monad as Monad (mapM_)
+import qualified Control.Monad.State as State (runStateT)
+import qualified System.Directory as Dir (getCurrentDirectory)
 
--- Small wrapper to handle parse errors, etc
+-- Small wrapper to handle parse errors, etc.
 runOpenscad :: [Char] -> Either Parsec.ParseError (IO (VarLookup, [SymbolicObj2], [SymbolicObj3]))
 runOpenscad s =
-	let
-		initial =  defaultObjects
-		rearrange (_, (varlookup, ovals, _ , _ , _)) = (varlookup, obj2s, obj3s) where
-			(obj2s, obj3s, _ ) = divideObjs ovals
-	in case parseProgram "" s of
-		Left e -> Left e
-		Right sts -> Right
-			$ fmap rearrange
-			$ (\sts -> do
-				path <- Dir.getCurrentDirectory
-				State.runStateT sts (initial, [], path, (), () )
-			)
-			$ Monad.mapM_ runStatementI sts
-
-
-
+  let
+    initial =  defaultObjects
+    rearrange (_, (varlookup, ovals, _ , _ , _)) = (varlookup, obj2s, obj3s) where
+                      (obj2s, obj3s, _ ) = divideObjs ovals
+  in case parseProgram [] s of
+    Left e -> Left e
+    Right sts -> Right
+	$ fmap rearrange
+	$ (\sts -> do
+	  path <- Dir.getCurrentDirectory
+	  State.runStateT sts (initial, [], path, (), () )
+	)
+	$ Monad.mapM_ runStatementI sts
