a575e83fb823b37d75835b599331cc6f3f543c5a
Andreas Rottmann <a.rottmann@gmx.at>
2015-12-08 16:58:17 +0100

    Add basic unit tests and benchmarks for parser
    The tests uncover a problem in the parser handling the ternary operator,
    the testcases that would fail are marked as pending (see 'ternaryIssue'
    in the code).
    
    The benchmarks indicate that the parser is unreasonably inefficient;
    here are is the result summary for my machine (Intel i5-6600K, 3.5GHz):
    
    - 5000 line comments, 80 chars per line: ~50ms
    - 500 block comments spanning 10 lines, 40 chars per line: ~58ms
    - 100 assignments ("x = (foo + bar);"): ~268ms
    - List of 1000 integers: ~1.1s
    - Arithmetic expressions with nesting level 3: ~2.3s
    
    Especially the integer list and arithmetic benchmark times seems way
    worse than they could/should be.
    
    Note that I already have a refactored parser in preparation that will
    hopefully do significantly better.

---
+++ bench/ParserBench.hs
@@ -0,0 +1,61 @@
+import Criterion.Main
+import Graphics.Implicit.ExtOpenScad.Definitions
+import Graphics.Implicit.ExtOpenScad.Parser.Expr
+import Graphics.Implicit.ExtOpenScad.Parser.Statement
+import Text.ParserCombinators.Parsec hiding (State)
+import Text.Printf
+
+lineComment :: Int -> String
+lineComment width = "//" ++ ['x' | _ <- [1..width]] ++ "\n"
+
+lineComments :: Int -> String
+lineComments n = concat [lineComment 80 | _ <- [1..n]]
+                 ++ assignments 1 -- to avoid empty file
+
+blockComment :: Int -> Int -> String
+blockComment lineCount width =
+  "/*" ++ concat [['x' | _ <- [1..width]] ++ "\n" | _ <- [1..lineCount]] ++ "*/"
+
+blockComments :: Int -> Int -> String
+blockComments lineCount n = concat [blockComment lineCount 40 | _ <- [1..n]]
+                            ++ assignments 1 -- to avoid empty file
+
+assignments :: Int -> String
+assignments n = concat ["x = (foo + bar);\n" | _ <- [1..n]]
+
+intList :: Int -> String
+intList n = "[" ++ concat [(show i) ++ "," | i <- [1..n]] ++ "0]"
+
+parseExpr :: String -> Expr
+parseExpr s = case parse expr0 "src" s of
+               Left err -> error (show err)
+               Right e -> e
+
+parseStatements :: String -> [StatementI]
+parseStatements s = case parseProgram "src" s of
+                     Left err -> error (show err)
+                     Right e -> e
+
+deepArithmetic :: Int -> String
+deepArithmetic n
+  | n == 0 = "1"
+  | otherwise = printf "%s + %s * (%s - %s)" d d d d
+                where
+                  d = deepArithmetic (n - 1)
+
+run :: String -> (String -> a) -> String -> Benchmark
+run name func input =
+  env (return $ input) $ \s ->
+  bench name $ whnf func s
+
+main :: IO ()
+main =
+  defaultMain $
+  [ bgroup "comments"
+    [ run "line" parseStatements (lineComments 5000)
+    , run "block" parseStatements (blockComments 10 500)
+    ]
+  , run "assignments" parseStatements (assignments 100)
+  , run "int list" parseExpr (intList 1000)
+  , run "deep arithmetic" parseExpr (deepArithmetic 3)
+  ]
