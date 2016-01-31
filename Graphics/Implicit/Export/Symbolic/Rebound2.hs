2ead8fc53e407e79faa4d4347aeb91f3e9c49360
Julia Longtin <julialongtin@diasp.org>
2016-01-31 01:47:38 +0000

    Spacing changes. spaces instead of tabs, and remove trailing whitespace.

--- Graphics/Implicit/Export/Symbolic/Rebound2.hs
+++ Graphics/Implicit/Export/Symbolic/Rebound2.hs
@@ -4,9 +4,9 @@ import Data.VectorSpace
 import Graphics.Implicit.Definitions
 
 rebound2 :: BoxedObj2 -> BoxedObj2
-rebound2 (obj, (a,b)) = 
-	let
-		d :: ℝ2
-		d = (b ^-^ a) ^/ 10
-	in 
-		(obj, ((a ^-^ d), (b ^+^ d)))
+rebound2 (obj, (a,b)) =
+    let
+        d :: ℝ2
+        d = (b ^-^ a) ^/ 10
+    in
+        (obj, ((a ^-^ d), (b ^+^ d)))
