d4388bee88530478ded6bd457a978a6bc4473a06
Julia Longtin <julialongtin@diasp.org>
2016-01-31 02:10:45 +0000

    Spacing changes. spaces instead of tabs, and remove trailing whitespace.

--- Graphics/Implicit/Export/Render/Definitions.hs
+++ Graphics/Implicit/Export/Render/Definitions.hs
@@ -9,7 +9,7 @@ import Control.DeepSeq
 -- We want a format that can represent squares/quads and triangles.
 -- So that we can merge squares and thereby reduces triangles.
 
--- Regarding Sq: Sq Basis@(b1,b2,b3) (Height on b3) 
+-- Regarding Sq: Sq Basis@(b1,b2,b3) (Height on b3)
 --                  (b1 pos 1, b2 pos 1) (b1 pos 2, b2 pos 2)
 
 data TriSquare = Sq (ℝ3,ℝ3,ℝ3) ℝ ℝ2 ℝ2 | Tris [Triangle]
@@ -17,6 +17,6 @@ data TriSquare = Sq (ℝ3,ℝ3,ℝ3) ℝ ℝ2 ℝ2 | Tris [Triangle]
 -- For use with Parallel.Strategies later
 
 instance NFData TriSquare where
-	rnf (Sq b z xS yS) = rnf (b,z,xS,yS)
-	rnf (Tris tris) = rnf tris
+    rnf (Sq b z xS yS) = rnf (b,z,xS,yS)
+    rnf (Tris tris) = rnf tris
 
