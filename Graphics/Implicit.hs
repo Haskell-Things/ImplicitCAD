2651f10371fd87849cf5e7d8ee9ddb672c52d603
Julia Longtin <julialongtin@diasp.org>
2016-01-31 03:39:30 +0000

    Spacing changes. remove trailing whitespace.

--- Graphics/Implicit.hs
+++ Graphics/Implicit.hs
@@ -4,7 +4,7 @@
 
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, FlexibleContexts, TypeSynonymInstances, UndecidableInstances #-}
 
-{- The purpose of this file is to pass on the functionality we want 
+{- The purpose of this file is to pass on the functionality we want
    to be accessible to an end user who is compiling objects using
    this haskell library. -}
 
@@ -54,7 +54,7 @@ import Graphics.Implicit.Primitives (translate, scale, complement, union, inters
 import Graphics.Implicit.ExtOpenScad (runOpenscad)
 
 -- Export functions, for writing files based on the result of operations on primitives
-import qualified Graphics.Implicit.Export as Export (writeSVG, writeSTL, writeBinSTL, writeOBJ, writeSCAD2, writeSCAD3, writeTHREEJS, writeGCodeHacklabLaser, writePNG) 
+import qualified Graphics.Implicit.Export as Export (writeSVG, writeSTL, writeBinSTL, writeOBJ, writeSCAD2, writeSCAD3, writeTHREEJS, writeGCodeHacklabLaser, writePNG)
 
 -- Datatypes/classes defining the world, or part of the world.
 import Graphics.Implicit.Definitions (SymbolicObj2, SymbolicObj3, ℝ)
@@ -69,7 +69,7 @@ writeOBJ    = Export.writeOBJ   :: ℝ -> FilePath -> SymbolicObj3 -> IO ()
 writeSCAD2  = Export.writeSCAD2 :: ℝ -> FilePath -> SymbolicObj2 -> IO ()
 writeSCAD3  = Export.writeSCAD3 :: ℝ -> FilePath -> SymbolicObj3 -> IO ()
 writeTHREEJS = Export.writeTHREEJS :: ℝ -> FilePath -> SymbolicObj3 -> IO ()
-writeGCodeHacklabLaser = Export.writeGCodeHacklabLaser :: ℝ -> FilePath -> SymbolicObj2 -> IO () 
+writeGCodeHacklabLaser = Export.writeGCodeHacklabLaser :: ℝ -> FilePath -> SymbolicObj2 -> IO ()
 writePNG2 = Export.writePNG :: ℝ -> FilePath -> SymbolicObj2  -> IO ()
 writePNG3 = Export.writePNG :: ℝ -> FilePath -> SymbolicObj3  -> IO ()
 
