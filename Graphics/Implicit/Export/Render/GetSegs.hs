ec755836aae06031aeb55eb6a9c155f92638f08d
Julia Longtin <julialongtin@diasp.org>
2016-01-31 02:27:55 +0000

    Spacing changes. spaces instead of tabs, and remove trailing whitespace.

--- Graphics/Implicit/Export/Render/GetSegs.hs
+++ Graphics/Implicit/Export/Render/GetSegs.hs
@@ -8,8 +8,8 @@ import Graphics.Implicit.Export.Render.RefineSegs (refine)
 import Graphics.Implicit.Export.Util (centroid)
 import Data.VectorSpace
 
-{- The goal of getSegs is to create polylines to separate 
-   the interior and exterior vertices of a square intersectiong
+{- The goal of getSegs is to create polylines to separate
+   the interior and exterior vertices of a square intersecting
    an object described by an implicit function.
 
       O.....O        O.....O
@@ -20,11 +20,11 @@ import Data.VectorSpace
       #.....#        #.....#
 
   An interior point is one at which obj is negative.
-  
+
   What are all the variables?
   ===========================
 
-  To allow data sharing, lots of values we 
+  To allow data sharing, lots of values we
   could calculate are instead arguments.
 
 
@@ -56,105 +56,105 @@ import Data.VectorSpace
 getSegs :: ℝ2 -> ℝ2 -> Obj2 -> (ℝ,ℝ,ℝ,ℝ) -> (ℝ,ℝ,ℝ,ℝ) -> [Polyline]
 {-- # INLINE getSegs #-}
 
-getSegs p1 p2 obj (x1y1, x2y1, x1y2, x2y2) (midx1V,midx2V,midy1V,midy2V) = 
-	let 
-		(x,y) = p1
+getSegs p1 p2 obj (x1y1, x2y1, x1y2, x2y2) (midx1V,midx2V,midy1V,midy2V) =
+    let
+        (x,y) = p1
 
-		-- Let's evaluate obj at a few points...
-		c = obj (centroid [p1,p2])
+        -- Let's evaluate obj at a few points...
+        c = obj (centroid [p1,p2])
 
-		(dx,dy) = p2 ^-^ p1
-		res = sqrt (dx*dy)
+        (dx,dy) = p2 ^-^ p1
+        res = sqrt (dx*dy)
 
-		midx1 = (x,      midx1V )
-		midx2 = (x + dx, midx2V )
-		midy1 = (midy1V , y )
-		midy2 = (midy2V, y + dy)
+        midx1 = (x,      midx1V )
+        midx2 = (x + dx, midx2V )
+        midy1 = (midy1V , y )
+        midy2 = (midy2V, y + dy)
 
-		notPointLine (np1:np2:[]) = np1 /= np2
+        notPointLine (np1:np2:[]) = np1 /= np2
 
-		-- takes straight lines between mid points and subdivides them to
-		-- account for sharp corners, etc.
+        -- takes straight lines between mid points and subdivides them to
+        -- account for sharp corners, etc.
 
-	in map (refine res obj) . filter (notPointLine) $ case (x1y2 <= 0, x2y2 <= 0,
-	                                                        x1y1 <= 0, x2y1 <= 0) of
+    in map (refine res obj) . filter (notPointLine) $ case (x1y2 <= 0, x2y2 <= 0,
+                                                            x1y1 <= 0, x2y1 <= 0) of
 
-		-- An important point here is orientation. If you imagine going along a
-		-- generated segment, the interior should be on the left-hand side.
+        -- An important point here is orientation. If you imagine going along a
+        -- generated segment, the interior should be on the left-hand side.
 
-		-- Empty Cases
+        -- Empty Cases
 
-		(True,  True, 
-		 True,  True)  -> []
+        (True,  True,
+         True,  True)  -> []
 
-		(False, False,
-		 False, False) -> []
+        (False, False,
+         False, False) -> []
 
-		-- Horizontal Cases
+        -- Horizontal Cases
 
-		(True,  True, 
-		 False, False) -> [[midx1, midx2]]
+        (True,  True,
+         False, False) -> [[midx1, midx2]]
 
-		(False, False,
-		 True,  True)  -> [[midx2, midx1]]
+        (False, False,
+         True,  True)  -> [[midx2, midx1]]
 
-		-- Vertical Cases
+        -- Vertical Cases
 
-		(False, True, 
-		 False, True)  -> [[midy2, midy1]]
+        (False, True,
+         False, True)  -> [[midy2, midy1]]
 
-		(True,  False,
-		 True,  False) -> [[midy1, midy2]]
+        (True,  False,
+         True,  False) -> [[midy1, midy2]]
 
-		-- Corner Cases
+        -- Corner Cases
 
-		(True,  False,
-		 False, False) -> [[midx1, midy2]]
+        (True,  False,
+         False, False) -> [[midx1, midy2]]
 
-		(False, True, 
-		 True,  True)  -> [[midy2, midx1]]
+        (False, True,
+         True,  True)  -> [[midy2, midx1]]
 
-		(True,  True, 
-		 False, True)  -> [[midx1, midy1]]
+        (True,  True,
+         False, True)  -> [[midx1, midy1]]
 
-		(False, False,
-		 True,  False) -> [[midy1, midx1]]
+        (False, False,
+         True,  False) -> [[midy1, midx1]]
 
-		(True,  True, 
-		 True,  False) -> [[midy1, midx2]]
+        (True,  True,
+         True,  False) -> [[midy1, midx2]]
 
-		(False, False,
-		 False, True)  -> [[midx2, midy1]]
+        (False, False,
+         False, True)  -> [[midx2, midy1]]
 
-		(True,  False,
-		 True,  True)  -> [[midx2, midy2]]
+        (True,  False,
+         True,  True)  -> [[midx2, midy2]]
 
-		(False, True, 
-		 False, False) -> [[midy2, midx2]]
+        (False, True,
+         False, False) -> [[midy2, midx2]]
 
-		-- Dual Corner Cases
+        -- Dual Corner Cases
 
-		(True,  False,
-		 False, True)  -> if c <= 0
-			then [[midx1, midy1], [midx2, midy2]]
-			else [[midx1, midy2], [midx2, midy1]]
+        (True,  False,
+         False, True)  -> if c <= 0
+            then [[midx1, midy1], [midx2, midy2]]
+            else [[midx1, midy2], [midx2, midy1]]
 
-		(False, True, 
-		 True,  False) -> if c <= 0
-			then [[midy2, midx1], [midy1, midx2]]
-			else [[midy1, midx1], [midy2, midx2]]
+        (False, True,
+         True,  False) -> if c <= 0
+            then [[midy2, midx1], [midy1, midx2]]
+            else [[midy1, midx1], [midy2, midx2]]
 
 
 -- A convenience function, we don't actually care too much about
 
 {-- # INLINE getSegs' #-}
 
-getSegs' (x1, y1) (x2, y2) obj (midx1V,midx2V,midy1V,midy2V) = 
-	let
-		x1y1 = obj (x1, y1)
-		x2y1 = obj (x2, y1)
-		x1y2 = obj (x1, y2)
-		x2y2 = obj (x2, y2)
-	in
-		getSegs (x1, y1) (x2, y2) obj (x1y1, x2y1, x1y2, x2y2) (midx1V,midx2V,midy1V,midy2V)
+getSegs' (x1, y1) (x2, y2) obj (midx1V,midx2V,midy1V,midy2V) =
+    let
+        x1y1 = obj (x1, y1)
+        x2y1 = obj (x2, y1)
+        x1y2 = obj (x1, y2)
+        x2y2 = obj (x2, y2)
+    in
+        getSegs (x1, y1) (x2, y2) obj (x1y1, x2y1, x1y2, x2y2) (midx1V,midx2V,midy1V,midy2V)
 
