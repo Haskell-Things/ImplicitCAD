9a105e823ec70467dcfc7b500198e7e14315da0e
Julia Longtin <julialongtin@diasp.org>
2016-01-31 06:38:20 +0000

    add mikes copyright, and change the spacing.

--- Graphics/Implicit/Export.hs
+++ Graphics/Implicit/Export.hs
@@ -1,5 +1,6 @@
 -- Implicit CAD. Copyright (C) 2011, Christopher Olah (chris@colah.ca)
 -- Copyright (C) 2014 2015, Julia Longtin (julial@turinglace.com)
+-- Copyright (C) 2015 2016, Mike MacHenry (mike.machenry@gmail.com)
 -- Released under the GNU GPL, see LICENSE
 
 {-# LANGUAGE FlexibleContexts #-}
@@ -32,36 +33,32 @@ import qualified Graphics.Implicit.Export.SymbolicFormats as SymbolicFormats
 import qualified Codec.Picture as ImageFormatCodecs
 
 -- Write an object using the given format function.
-writeObject :: (DiscreteAproxable obj aprox) => 
-               ℝ                   -- ^ Resolution
-               -> (aprox -> Text)  -- ^ File Format (Function that formats)
-               -> FilePath         -- ^ File Name
-               -> obj              -- ^ Object to render
-               -> IO ()            -- ^ Writing Action!
+writeObject :: (DiscreteAproxable obj aprox)
+    => ℝ                -- ^ Resolution
+    -> (aprox -> Text)  -- ^ File Format (Function that formats)
+    -> FilePath         -- ^ File Name
+    -> obj              -- ^ Object to render
+    -> IO ()            -- ^ Writing Action!
 writeObject res format filename obj =
-  let
-    aprox = formatObject res format obj
-  in
-   LT.writeFile filename aprox
+    let aprox = formatObject res format obj
+    in LT.writeFile filename aprox
 
 -- Write an object using the given format writer.
-writeObject' :: (DiscreteAproxable obj aprox) => 
-                ℝ                   -- ^ Resolution
-                -> (FilePath -> aprox -> IO ())  -- ^ File Format writer
-                -> FilePath         -- ^ File Name
-                -> obj              -- ^ Object to render
-                -> IO ()            -- ^ Writing Action!
+writeObject' :: (DiscreteAproxable obj aprox)
+    => ℝ                -- ^ Resolution
+    -> (FilePath -> aprox -> IO ())  -- ^ File Format writer
+    -> FilePath         -- ^ File Name
+    -> obj              -- ^ Object to render
+    -> IO ()            -- ^ Writing Action!
 writeObject' res formatWriter filename obj =
-  let
-    aprox = discreteAprox res obj
-  in
-   formatWriter filename aprox
+    let aprox = discreteAprox res obj
+    in formatWriter filename aprox
 
-formatObject :: (DiscreteAproxable obj aprox) =>
-                ℝ                   -- ^ Resolution
-                -> (aprox -> Text)  -- ^ File Format (Function that formats)
-                -> obj              -- ^ Object to render
-                -> Text             -- ^ Resulting lazy ByteString
+formatObject :: (DiscreteAproxable obj aprox)
+    => ℝ                -- ^ Resolution
+    -> (aprox -> Text)  -- ^ File Format (Function that formats)
+    -> obj              -- ^ Object to render
+    -> Text             -- ^ Resulting lazy ByteString
 formatObject res format = format . discreteAprox res
 
 writeSVG res = writeObject res PolylineFormats.svg
