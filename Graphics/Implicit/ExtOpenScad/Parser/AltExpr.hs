e8e561e3a97dbff762f6cf6cc790b9e1ed856326
Kelvin Cookshaw <kelvin@cookshaw.com>
2016-07-17 13:52:15 -0600

    Mods to AltExpr parser: Added function call expressions, including calls that allow an expression instead of an identifier to specify the function. Changed parser to report an error with "an expression", rather than stating what tokens it was expecting. Export expr0, so it can be used by Statement.hs for testing. Fixed parentheses expressions, which were formerly empty. Fixed type declarations to be compatible with the existing expressions in Expr.hs. Messed around with spacing in the Haskell source. Not sure how the code should look.

--- Graphics/Implicit/ExtOpenScad/Parser/AltExpr.hs
+++ Graphics/Implicit/ExtOpenScad/Parser/AltExpr.hs
@@ -1,4 +1,4 @@
-module Graphics.Implicit.ExtOpenScad.Parser.AltExpr where
+module Graphics.Implicit.ExtOpenScad.Parser.AltExpr (expr0) where
 
 -- TODO remove tracing
 import Debug.Trace;
@@ -7,6 +7,11 @@ import Text.ParserCombinators.Parsec  hiding (State)
 import Graphics.Implicit.ExtOpenScad.Definitions
 import Graphics.Implicit.ExtOpenScad.Parser.Lexer
 
+expr0 = do
+        expr
+    <?> "an expression"
+
+nonOperator :: GenParser Char st Expr
 nonOperator = do
         matchTrue
         return $ LitE $ OBool True
@@ -29,19 +34,19 @@ nonOperator = do
         return $ Var ident
     <|> do
         _ <- matchChar '('
+        expr <- expr
         _ <- matchChar ')'
-        return $ LitE $ OString "()"
-
+        return expr
+    <?> "an expression"
 binaryOperation :: String -> Expr -> Expr -> Expr
 binaryOperation symbol left right = Var symbol :$ [left, right]
 
-assignment :: Parser Expr
-assignment = (do
+assignment :: GenParser Char st Expr
+assignment = do
     ident <- identifier
     matchChar '='
     expr <- expr
     return $ ListE [Var ident, expr]
-    )
 
 bindLets :: Expr -> Expr -> Expr
 bindLets (ListE [Var boundName, boundExpr]) nestedExpr = (LamE [Name boundName] nestedExpr) :$ [boundExpr]
@@ -55,66 +60,70 @@ expr = foldr ($) nonOperator levels
     where
         levels = 
           [ id
-          , \higher -> fix $ \self -> -- "let" expression
-                do
-                        matchLet
-                        matchChar '('
-                        bindings <- sepBy assignment (matchChar ',')
-                        matchChar ')'
-                        expr <- self
-                        return $ foldr bindLets expr bindings
-                <|> higher
-          , \higher -> fix $ \self -> -- ternary operator, ? :
-                do  condition <- higher 
-                    ( 
-                        do
-                            matchChar '?'
-                            trueExpr <- self
-                            matchChar ':'
-                            falseExpr <- self
-                            return $ Var "?" :$ [condition, trueExpr, falseExpr]
-                        <|> return condition
-                        )
-          , \higher -> -- || operator
-                do
-                    chainl1 higher (do
-                        op <- matchOR
-                        return $ binaryOperation op
-                        )
-          , \higher -> -- && operator
-                do
-                    chainl1 higher (do
-                        op <- matchAND
-                        return $ binaryOperation op
-                        )
-          , \higher -> -- <, <=, >=, > comparison operators
-                do
-                    chainl1 higher (do
-                        op <- matchChar '<' <|> matchLE <|> matchGE <|> matchChar '>'
-                        return $ binaryOperation op
-                        )
-          , \higher -> -- == and != operators
-                do
-                    chainl1 higher (do
-                        op <-(matchEQ <|> matchNE)
-                        return $ binaryOperation op
-                        )
-          , \higher -> -- + and - operators
-                do
-                    chainl1 higher (do
-                        op <- matchChar '-' <|> matchChar '+'
-                        return $ binaryOperation op
-                        )
-          , \higher -> fix $ \self -> do -- OpenSCAD's YACC parser puts '!' at the same level of precedence as '-' and '+'. I think the semantics are the same.
+          , \higher -> fix $ \self -> do -- "let" expression
+                matchLet
+                matchChar '('
+                bindings <- sepBy assignment (matchChar ',')
+                matchChar ')'
+                expr <- self
+                return $ foldr bindLets expr bindings
+            <|>
+                higher
+
+          , \higher -> fix $ \self -> do -- ternary operator, ? :
+               condition <- higher 
+               (do
+                    matchChar '?'
+                    trueExpr <- self
+                    matchChar ':'
+                    falseExpr <- self
+                    return $ Var "?" :$ [condition, trueExpr, falseExpr]
+                <|>
+                    return condition)
+
+          , \higher -> do -- || operator
+                chainl1 higher (do
+                    op <- matchOR
+                    return $ binaryOperation op)
+                    
+          , \higher -> do -- && operator
+                chainl1 higher (do
+                    op <- matchAND
+                    return $ binaryOperation op)
+                    
+          , \higher -> do -- <, <=, >=, > comparison operators
+                chainl1 higher (do
+                    op <- matchChar '<' <|> matchLE <|> matchGE <|> matchChar '>'
+                    return $ binaryOperation op)
+                    
+          , \higher -> do -- == and != operators
+                chainl1 higher (do
+                    op <-(matchEQ <|> matchNE)
+                    return $ binaryOperation op)
+                    
+          , \higher -> do -- + and - operators
+                chainl1 higher (do
+                    op <- matchChar '-' <|> matchChar '+'
+                    return $ binaryOperation op)
+                    
+          , \higher -> fix $ \self -> -- OpenSCAD's YACC parser puts '!' at the same level of precedence as '-' and '+'. I think the semantics are the same.
                 do
                     bang <- matchChar '!'
                     right <- self
                     return $ Var bang :$ [right]
                 <|> higher
-          , \higher -> -- *, /, % operators
-                do
-                    chainl1 higher (do
+
+          , \higher -> do -- *, /, % operators
+                chainl1 higher (do
                         op <- matchChar '*' <|> matchChar '/' <|> matchChar '%'
-                        return $ binaryOperation op
-                        )
-            ]
+                        return $ binaryOperation op)
+
+          , \higher -> -- function call - in OpenSCAD a function call can only happen to a indentifier. In ExtOpenScad a function call can happen to any expression that returns a function (or lambda expression)
+                    do func <- higher
+                       (do
+                            matchChar '(' <?> "func '('"
+                            arguments <- sepBy expr (matchChar ',')
+                            matchChar ')'
+                            return $ func :$ arguments
+                        <|> return func)
+          ]
