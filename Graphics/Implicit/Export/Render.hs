7011bdabae656cc1df4634e64508983b23856a95
Julia Longtin <julialongtin@diasp.org>
2016-10-16 17:07:29 +0100

    use fromInteger, remove some unnecessary fromIntegers, remove an unnecessary parameter from cleanupTris.

--- Graphics/Implicit/Export/Render.hs
+++ Graphics/Implicit/Export/Render.hs
@@ -93,42 +93,39 @@ getMesh p1@(x1,y1,z1) p2 res obj =
         {-# INLINE par3DList #-}
         par3DList lenx leny lenz f =
             [[[f
-                (\n -> x1 + rx*fromIntegral (mx+n)) mx
-                (\n -> y1 + ry*fromIntegral (my+n)) my
-                (\n -> z1 + rz*fromIntegral (mz+n)) mz
+                (\n -> x1 + rx*fromInteger (mx+n)) mx
+                (\n -> y1 + ry*fromInteger (my+n)) my
+                (\n -> z1 + rz*fromInteger (mz+n)) mz
             | mx <- [0..lenx] ] | my <- [0..leny] ] | mz <- [0..lenz] ]
-                `using` (parListChunk (max 1 $ div lenz 32) rdeepseq)
+                `using` (parListChunk (max 1 $ fromInteger $ div lenz 32) rdeepseq)
 
 
         -- Evaluate obj to avoid waste in mids, segs, later.
-
         objV = par3DList (nx+2) (ny+2) (nz+2) $ \x _ y _ z _ -> obj (x 0, y 0, z 0)
 
         -- (1) Calculate mid points on X, Y, and Z axis in 3D space.
-
         midsZ = [[[
                  interpolate (z0, objX0Y0Z0) (z1, objX0Y0Z1) (appAB obj x0 y0) res
                  | x0 <- pXs |                  objX0Y0Z0 <- objY0Z0 | objX0Y0Z1 <- objY0Z1
                 ]| y0 <- pYs |                  objY0Z0 <- objZ0 | objY0Z1 <- objZ1
                 ]| z0 <- pZs | z1 <- tail pZs | objZ0   <- objV  | objZ1   <- tail objV
-                ] `using` (parListChunk (max 1 $ div nz 32) rdeepseq)
+                ] `using` (parListChunk (max 1 $ fromInteger $ div nz 32) rdeepseq)
 
         midsY = [[[
                  interpolate (y0, objX0Y0Z0) (y1, objX0Y1Z0) (appAC obj x0 z0) res
                  | x0 <- pXs |                  objX0Y0Z0 <- objY0Z0 | objX0Y1Z0 <- objY1Z0
                 ]| y0 <- pYs | y1 <- tail pYs | objY0Z0 <- objZ0 | objY1Z0 <- tail objZ0
                 ]| z0 <- pZs |                  objZ0   <- objV
-                ] `using` (parListChunk (max 1 $ div nz 32) rdeepseq)
+                ] `using` (parListChunk (max 1 $ fromInteger $ div nz 32) rdeepseq)
 
         midsX = [[[
                  interpolate (x0, objX0Y0Z0) (x1, objX1Y0Z0) (appBC obj y0 z0) res
                  | x0 <- pXs | x1 <- tail pXs | objX0Y0Z0 <- objY0Z0 | objX1Y0Z0 <- tail objY0Z0
                 ]| y0 <- pYs |                  objY0Z0 <- objZ0
                 ]| z0 <- pZs |                  objZ0   <- objV
-                ] `using` (parListChunk (max 1 $ div nz 32) rdeepseq)
+                ] `using` (parListChunk (max 1 $ fromInteger $ div nz 32) rdeepseq)
 
         -- Calculate segments for each side
-
         segsZ = [[[
             map2  (inj3 z0) $ getSegs (x0,y0) (x1,y1) (obj **$ z0)
                 (objX0Y0Z0, objX1Y0Z0, objX0Y1Z0, objX1Y1Z0)
@@ -139,7 +136,7 @@ getMesh p1@(x1,y1,z1) p2 res obj =
              |objY0Z0 <- objZ0 | objY1Z0 <- tail objZ0
             ]|z0<-pZs             |mX'  <-midsX|                mY'  <-midsY
              |objZ0 <- objV
-            ] `using` (parListChunk (max 1 $ div nz 32) rdeepseq)
+            ] `using` (parListChunk (max 1 $ fromInteger $ div nz 32) rdeepseq)
 
         segsY = [[[
             map2  (inj2 y0) $ getSegs (x0,z0) (x1,z1) (obj *$* y0)
@@ -151,7 +148,7 @@ getMesh p1@(x1,y1,z1) p2 res obj =
              |objY0Z0 <- objZ0 | objY0Z1 <- objZ1
             ]|z0<-pZs|z1<-tail pZs|mB'  <-midsX|mBT  <-tail midsX|mA'  <-midsZ
              |objZ0 <- objV | objZ1 <- tail objV
-            ] `using` (parListChunk (max 1 $ div nz 32) rdeepseq)
+            ] `using` (parListChunk (max 1 $ fromInteger $ div nz 32) rdeepseq)
 
         segsX =
             [[[
@@ -164,10 +161,9 @@ getMesh p1@(x1,y1,z1) p2 res obj =
              |objY0Z0  <-objZ0  |objY1Z0  <-tail objZ0  |objY0Z1  <-objZ1  |objY1Z1  <-tail objZ1
             ]|z0<-pZs|z1<-tail pZs|mB'  <-midsY|mBT  <-tail midsY|mA'  <-midsZ
              |objZ0 <- objV | objZ1 <- tail objV
-            ]  `using` (parListChunk (max 1 $ div nz 32) rdeepseq)
+            ]  `using` (parListChunk (max 1 $ fromInteger $ div nz 32) rdeepseq)
 
         -- (3) & (4) : get and tesselate loops
-
         sqTris = [[[
             concat $ map (tesselateLoop res obj) $ getLoops $ concat [
                         segX''',
@@ -190,14 +186,12 @@ getMesh p1@(x1,y1,z1) p2 res obj =
              | segY' <- segsY
              | segX' <- segsX
             ]
-
-    in cleanupTris res $ mergedSquareTris $ concat $ concat $ concat sqTris -- (5) merge squares, etc
-    
+    in cleanupTris $ mergedSquareTris $ concat $ concat $ concat sqTris -- (5) merge squares, etc
 
 -- Removes triangles that are empty, when converting their positions to Float resolution.
--- NOTE: this will need to be removed for AMF, or other triangle formats that can handle Double.
-cleanupTris :: ℝ -> TriangleMesh -> TriangleMesh
-cleanupTris res tris =
+-- NOTE: this will need to be disabled for AMF, and other triangle formats that can handle Double.
+cleanupTris :: TriangleMesh -> TriangleMesh
+cleanupTris tris =
     let
         toFloat = realToFrac :: (Real a) => a -> Float
         floatPoint (a,b,c) = (toFloat a, toFloat b, toFloat c)
@@ -228,7 +222,7 @@ getContour p1@(x1, y1) p2@(_, _) res obj =
                 (\n -> x1 + rx*fromIntegral (mx+n)) mx
                 (\n -> y1 + ry*fromIntegral (my+n)) my
             | mx <- [0..lenx] ] | my <- [0..leny] ]
-                `using` (parListChunk (max 1 $ div leny 32) rdeepseq)
+                `using` (parListChunk (max 1 $ fromInteger $ div leny 32) rdeepseq)
 
 
         -- Evaluate obj to avoid waste in mids, segs, later.
@@ -241,13 +235,13 @@ getContour p1@(x1, y1) p2@(_, _) res obj =
                  interpolate (y0, objX0Y0) (y1, objX0Y1) (obj $* x0) res
                  | x0 <- pXs |                  objX0Y0 <- objY0   | objX0Y1 <- objY1
                 ]| y0 <- pYs | y1 <- tail pYs | objY0   <- objV    | objY1   <- tail objV
-                ] `using` (parListChunk (max 1 $ div ny 32) rdeepseq)
+                ] `using` (parListChunk (max 1 $ fromInteger $ div ny 32) rdeepseq)
 
         midsX = [[
                  interpolate (x0, objX0Y0) (x1, objX1Y0) (obj *$ y0) res
                  | x0 <- pXs | x1 <- tail pXs | objX0Y0 <- objY0 | objX1Y0 <- tail objY0
                 ]| y0 <- pYs |                  objY0   <- objV
-                ] `using` (parListChunk (max 1 $ div ny 32) rdeepseq)
+                ] `using` (parListChunk (max 1 $ fromInteger $ div ny 32) rdeepseq)
 
         -- Calculate segments for each side
 
@@ -259,36 +253,52 @@ getContour p1@(x1, y1) p2@(_, _) res obj =
              |objX0Y0<-objY0|objX1Y0<-tail objY0|objX0Y1<-objY1|objX1Y1<-tail objY1
             ]|y0<-pYs|y1<-tail pYs|mX'' <-midsX|mX'T <-tail midsX|mY'' <-midsY
              |objY0 <- objV  | objY1 <- tail objV
-            ] `using` (parListChunk (max 1 $ div ny 32) rdeepseq)
+            ] `using` (parListChunk (max 1 $ fromInteger $ div ny 32) rdeepseq)
 
     in cleanLoopsFromSegs $ concat $ concat $ segs -- (5) merge squares, etc
 
 
 
 
--- silly utility functions
+-- utility functions
 
+inj1 :: forall t t1 t2. t -> (t1, t2) -> (t, t1, t2)
 inj1 a (b,c) = (a,b,c)
+inj2 :: forall t t1 t2. t1 -> (t, t2) -> (t, t1, t2)
 inj2 b (a,c) = (a,b,c)
+inj3 :: forall t t1 t2. t2 -> (t, t1) -> (t, t1, t2)
 inj3 c (a,b) = (a,b,c)
 
+($**) :: forall t t1 t2 t3. ((t1, t2, t3) -> t) -> t1 -> (t2, t3) -> t
 infixr 0 $**
+(*$*) :: forall t t1 t2 t3. ((t1, t2, t3) -> t) -> t2 -> (t1, t3) -> t
 infixr 0 *$*
+(**$) :: forall t t1 t2 t3. ((t1, t2, t3) -> t) -> t3 -> (t1, t2) -> t
 infixr 0 **$
+
+($*) :: forall t t1 t2. ((t1, t2) -> t) -> t1 -> t2 -> t
 infixr 0 $*
+(*$) :: forall t t1 t2. ((t1, t2) -> t) -> t2 -> t1 -> t
 infixr 0 *$
+
 f $* a = \b -> f (a,b)
 f *$ b = \a -> f (a,b)
 f $** a = \(b,c) -> f (a,b,c)
 f *$* b = \(a,c) -> f (a,b,c)
 f **$ c = \(a,b) -> f (a,b,c)
 
+appAB :: forall t t1 t2 t3. ((t1, t2, t3) -> t) -> t1 -> t2 -> t3 -> t
 appAB f a b = \c -> f (a,b,c)
+appBC :: forall t t1 t2 t3. ((t1, t2, t3) -> t) -> t2 -> t3 -> t1 -> t
 appBC f b c = \a -> f (a,b,c)
+appAC :: forall t t1 t2 t3. ((t1, t2, t3) -> t) -> t1 -> t3 -> t2 -> t
 appAC f a c = \b -> f (a,b,c)
 
+map2 :: forall a b. (a -> b) -> [[a]] -> [[b]]
 map2 f = map (map f)
+map2R :: forall a a1. (a1 -> a) -> [[a1]] -> [[a]]
 map2R f = map (reverse . map f)
+mapR :: forall a. [[a]] -> [[a]]
 mapR = map reverse
 
 {-
