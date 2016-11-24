3a6bb2400f0546b8c73458d2081013a825e72e0e
Julia Longtin <julialongtin@diasp.org>
2016-11-24 04:02:45 +0000

    change the form of handling functions without rotation, and use error to highlight using functions that do not handle rotation.

--- Graphics/Implicit/Export/SymbolicFormats.hs
+++ Graphics/Implicit/Export/SymbolicFormats.hs
@@ -23,6 +23,10 @@ scad2 res obj = toLazyText $ runReader (buildS2 obj) res
 scad3 :: ℝ -> SymbolicObj3 -> Text 
 scad3 res obj = toLazyText $ runReader (buildS3 obj) res
 
+-- used by rotate2 and rotate3
+rad2deg :: ℝ -> ℝ
+rad2deg r = r * (180/pi)
+
 -- Format an openscad call given that all the modified objects are in the Reader monad...
 
 callToken :: (Text, Text) -> Builder -> [Builder] -> [Reader a Builder] -> Reader a Builder
@@ -45,7 +49,7 @@ callNaked = callToken ("", "")
 -- First, the 3D objects.
 buildS3 :: SymbolicObj3 -> Reader ℝ Builder
 
-buildS3 (Rect3R 0 (x1,y1,z1) (x2,y2,z2)) = call "translate" [bf x1, bf y1, bf z1] [
+buildS3 (Rect3R r (x1,y1,z1) (x2,y2,z2)) | r == 0 = call "translate" [bf x1, bf y1, bf z1] [
                                             call "cube" [bf $ x2 - x1, bf $ y2 - y1, bf $ z2 - z1] []
                                            ]
 
@@ -59,34 +63,32 @@ buildS3 (Cylinder h r1 r2) = call "cylinder" [
 
 buildS3 (Complement3 obj) = call "complement" [] [buildS3 obj]
 
-buildS3 (UnionR3 0 objs) = call "union" [] $ map buildS3 objs
+buildS3 (UnionR3 r objs) | r == 0 = call "union" [] $ map buildS3 objs
 
-buildS3 (IntersectR3 0 objs)  = call "intersection" [] $ map buildS3 objs
+buildS3 (IntersectR3 r objs) | r == 0 = call "intersection" [] $ map buildS3 objs
 
-buildS3 (DifferenceR3 0 objs) = call "difference" [] $ map buildS3 objs
+buildS3 (DifferenceR3 r objs) | r == 0 = call "difference" [] $ map buildS3 objs
 
 buildS3 (Translate3 (x,y,z) obj) = call "translate" [bf x, bf y, bf z] [buildS3 obj]
 
 buildS3 (Scale3 (x,y,z) obj) = call "scale" [bf x, bf y, bf z] [buildS3 obj]
 
 buildS3 (Rotate3 (x,y,z) obj) = call "rotate" [bf (rad2deg x), bf (rad2deg y), bf (rad2deg z)] [buildS3 obj]
-    where
-        rad2deg r = r * (180/pi)
 
 -- FIXME: where is Rotate3V?
+buildS3 (Rotate3V _ _ _) = error "Rotate3V not implemented."
 
-buildS3 (Outset3 0 obj) = call "outset" [] [buildS3 obj]
+buildS3 (Outset3 r obj) | r == 0 = call "outset" [] [buildS3 obj]
 
-buildS3 (Shell3 0 obj) = call "shell" [] [buildS3 obj]
+buildS3 (Shell3 r obj) | r == 0 = call "shell" [] [buildS3 obj]
 
 -- FIXME: where is EmbedBoxedObj3?
 
-buildS3 (ExtrudeR 0 obj h) = callNaked "linear_extrude" ["height = " <> bf h] [buildS2 obj]
+buildS3 (ExtrudeR r obj h) | r == 0 = callNaked "linear_extrude" ["height = " <> bf h] [buildS2 obj]
 
-buildS3 (ExtrudeRotateR 0 twist obj h) =
-    callNaked "linear_extrude" ["height = " <> bf h, "twist = " <> bf twist] [buildS2 obj]
+buildS3 (ExtrudeRotateR r twist obj h) | r == 0 = callNaked "linear_extrude" ["height = " <> bf h, "twist = " <> bf twist] [buildS2 obj]
 
-buildS3 (ExtrudeRM 0 (Just twist) Nothing Nothing obj (Left height)) = do
+buildS3 (ExtrudeRM r (Just twist) Nothing Nothing obj (Left height)) | r == 0 = do
   res <- ask
   call "union" [] [
              call "rotate" ["0","0", bf $ twist h] [
@@ -98,38 +100,59 @@ buildS3 (ExtrudeRM 0 (Just twist) Nothing Nothing obj (Left height)) = do
 
 -- FIXME: where are RotateExtrude, ExtrudeOnEdgeOf?
 
+buildS3(Rect3R _ _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(UnionR3 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(IntersectR3 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(DifferenceR3 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(Outset3 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(Shell3 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(ExtrudeR _ _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(ExtrudeRotateR _ _ _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(ExtrudeRM _ _ _ _ _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(EmbedBoxedObj3 _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(RotateExtrude _ _ _ _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS3(ExtrudeOnEdgeOf _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+
 -- Now the 2D objects/transforms.
 
 buildS2 :: SymbolicObj2 -> Reader ℝ Builder
 
-buildS2 (RectR 0 (x1,y1) (x2,y2)) = call "translate" [bf x1, bf y1] [
+buildS2 (RectR r (x1,y1) (x2,y2)) | r == 0 = call "translate" [bf x1, bf y1] [
                                     call "cube" [bf $ x2 - x1, bf $ y2 - y1] []
                                    ]
 
 buildS2 (Circle r) = call "circle" [bf r] []
 
-buildS2 (PolygonR 0 points) = call "polygon" [buildVector [x,y] | (x,y) <- points] []
+buildS2 (PolygonR r points) | r == 0 = call "polygon" [buildVector [x,y] | (x,y) <- points] []
     where buildVector comps = "[" <> mconcat (intersperse "," $ map bf comps) <> "]"
 
 buildS2 (Complement2 obj) = call "complement" [] $ [buildS2 obj]
 
-buildS2 (UnionR2 0 objs)       = call "union" [] $ map buildS2 objs
+buildS2 (UnionR2 r objs) | r == 0 = call "union" [] $ map buildS2 objs
 
-buildS2 (DifferenceR2 0 objs)  = call "difference" [] $ map buildS2 objs
+buildS2 (DifferenceR2 r objs) | r == 0 = call "difference" [] $ map buildS2 objs
 
-buildS2 (IntersectR2 0 objs)   = call "intersection" [] $ map buildS2 objs
+buildS2 (IntersectR2 r objs) | r == 0 = call "intersection" [] $ map buildS2 objs
 
 buildS2 (Translate2 (x,y) obj) = call "translate" [bf x, bf y] $ [buildS2 obj]
 
 buildS2 (Scale2 (x,y) obj)     = call "scale" [bf x, bf y] $ [buildS2 obj]
 
 buildS2 (Rotate2 (r) obj)     = call "rotate" [bf (rad2deg r)] $ [buildS2 obj]
-    where
-        rad2deg myr = myr * (180/pi)
 
-buildS2 (Outset2 0 obj) = call "outset" [] $ [buildS2 obj]
+buildS2 (Outset2 r obj) | r == 0 = call "outset" [] $ [buildS2 obj]
 
-buildS2 (Shell2 0 obj) = call "shell" [] $ [buildS2 obj]
+buildS2 (Shell2 r obj) | r == 0 =  call "shell" [] $ [buildS2 obj]
+
+-- Generate errors for rounding requests. OpenSCAD does not support rounding.
+buildS2 (RectR _ _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS2 (PolygonR _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS2 (UnionR2 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS2 (DifferenceR2 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS2 (IntersectR2 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS2 (Outset2 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
+buildS2 (Shell2 _ _) = error "cannot provide roundness when exporting openscad; unsupported in target format."
 
 -- FIXME: missing EmbedBoxedObj2?
+buildS2 (EmbedBoxedObj2 _) = error "EmbedBoxedObj2 not implemented."
 
