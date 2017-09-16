d8c711460b62cf77c3ce8d7be9d51b4e84e60e76
Julia Longtin <julialongtin@diasp.org>
2017-09-16 06:08:16 +0100

    implement scientific notion support, convert $ to ., and remove unneeded $ and ()s.

--- Graphics/Implicit/ExtOpenScad/Parser/Expr.hs
+++ Graphics/Implicit/ExtOpenScad/Parser/Expr.hs
@@ -4,7 +4,7 @@
 
 module Graphics.Implicit.ExtOpenScad.Parser.Expr where
 
-import Prelude (Char, Maybe(Nothing, Just), fmap, ($), (>>), return, Bool(True, False), read, (++), id, foldl, map, foldl1, unzip, tail, zipWith3)
+import Prelude (Char, Maybe(Nothing, Just), fmap, ($), (.), (>>), return, Bool(True, False), read, (++), (*), (**), (/), id, foldl, map, foldl1, unzip, tail, zipWith3)
 
 -- the datatype representing the graininess of our world.
 import Graphics.Implicit.Definitions (ℝ)
@@ -13,6 +13,7 @@ import Graphics.Implicit.Definitions (ℝ)
 import Text.ParserCombinators.Parsec (GenParser, string, many1, digit, char, many, noneOf, sepBy, sepBy1, optionMaybe, try)
 
 import Graphics.Implicit.ExtOpenScad.Definitions (Expr(Var, LitE, ListE, (:$)), OVal(ONum, OString, OBool, OUndefined), collector) 
+
 import Graphics.Implicit.ExtOpenScad.Parser.Util (variableSymb, (?:), (*<|>), genSpace, padString)
 
 variable :: GenParser Char st Expr
@@ -23,27 +24,66 @@ literal = ("literal" ?:) $
     "boolean" ?: do
         b  <-      (string "true"  >> return True )
               *<|> (string "false" >> return False)
-        return $ LitE $ OBool b
+        return . LitE $ OBool b
+    -- FIXME: this is a hack, implement something like exprN to replace this?
     *<|> "number" ?: (
-        do
+         do
+            a <- many1 digit
+            _ <- char 'e'
+            b <- many1 digit
+            return . LitE $ ONum (((read a) * (10 ** (read b))) :: ℝ)
+        *<|>  do
             a <- many1 digit
             _ <- char '.'
             b <- many digit
-            return $ LitE $ ONum (read (a ++ "." ++ b) :: ℝ)
+            _ <- char 'e'
+            c <- many1 digit
+            return . LitE $ ONum ((read (a ++ "." ++ b) * (10 ** (read c))) :: ℝ)
         *<|>  do
             a <- many1 digit
-            return $ LitE $ ONum (read a :: ℝ)
+            _ <- char '.'
+            b <- many digit
+            _ <- char 'e'
+            _ <- char '+'
+            c <- many1 digit
+            return . LitE $ ONum ((read (a ++ "." ++ b) * (10 ** (read c))) :: ℝ)
+        *<|>  do
+            a <- many1 digit
+            _ <- char '.'
+            b <- many digit
+            _ <- char 'e'
+            _ <- char '-'
+            c <- many1 digit
+            return . LitE $ ONum ((read (a ++ "." ++ b) / (10 ** (read c))) :: ℝ)
+        *<|>  do
+            a <- many1 digit
+            _ <- char 'e'
+            _ <- char '-'
+            b <- many1 digit
+            return . LitE $ ONum (((read a) / (10 ** (read b))) :: ℝ)
+        *<|>  do
+            a <- many1 digit
+            _ <- char '.'
+            b <- many digit
+            return . LitE $ ONum (read (a ++ "." ++ b) :: ℝ)
+        *<|>  do
+            a <- many1 digit
+            return . LitE $ ONum (read a :: ℝ)
         )
-    *<|> "string" ?: do
+     *<|> "string" ?: do
         _ <- string "\""
         strlit <-  many $ (string "\\\"" >> return '\"')
                      *<|> (string "\\n" >> return '\n')
-                     *<|> ( noneOf "\"\n")
+                     *<|> (string "\\r" >> return '\r')
+                     *<|> (string "\\t" >> return '\t')
+                     *<|> (string "\\\\" >> return '\\')
+                      -- FIXME: no \u unicode support?
+                     *<|> noneOf "\"\n"
         _ <- string "\""
-        return $ LitE $ OString strlit
+        return . LitE $ OString strlit
 
 -- We represent the priority or 'fixity' of different types of expressions
--- by the Int argument
+-- by the ExprIdx argument, with A0 as the highest.
 
 expr0 :: GenParser Char st Expr
 expr0 = exprN A0
@@ -85,7 +125,7 @@ exprN A12 =
 
 exprN A11 =
     do
-        obj <- exprN $ A12
+        obj <- exprN A12
         _ <- genSpace
         mods <- many1 (
             "function application" ?: do
@@ -111,67 +151,73 @@ exprN A11 =
                     (Just s,  Just e )  -> \l -> Var "splice" :$ [l, s, e]
             )
         return $ foldl (\a b -> b a) obj mods
-    *<|> (exprN $ A12 )
+    *<|> exprN A12
 
+-- match a leading (+) or (-) operator.
 exprN A10 =
     "negation" ?: do
         _ <- padString "-"
-        expr <- exprN $ A11
+        expr <- exprN A11
         return $ Var "negate" :$ [expr]
     *<|> do
         _ <- padString "+"
-        expr <- exprN $ A11
-        return expr
-    *<|> exprN (A11)
+        exprN A11
+    *<|> exprN A11
 
+-- match power-of (^) operator.
 exprN A9 =
     "exponentiation" ?: do
-        a <- exprN $ A10
+        a <- exprN A10
         _ <- padString "^"
         b <- exprN A9
         return $ Var "^" :$ [a,b]
-    *<|> exprN (A10)
+    *<|> exprN A10
 
+-- match sequences of multiplication and division.
 exprN A8 =
     "multiplication/division" ?: do
         -- outer list is multiplication, inner division.
         -- eg. "1*2*3/4/5*6*7/8"
         --     [[1],[2],[3,4,5],[6],[7,8]]
         exprs <- sepBy1
-            (sepBy1 (exprN $ A9) (try $ padString "/" ))
+            (sepBy1 (exprN A9) (try $ padString "/" ))
             (try $ padString "*" )
         let div' a b = Var "/" :$ [a, b]
-        return $ collector "*" $ map (foldl1 div') exprs
-    *<|> exprN (A9)
+        return . collector "*" $ map (foldl1 div') exprs
+    *<|> exprN A9
 
+-- match remainder (%) operator.
 exprN A7 =
     "modulo" ?: do
-        exprs <- sepBy1 (exprN $ A8) (try $ padString "%")
+        exprs <- sepBy1 (exprN  A8) (try $ padString "%")
         let mod' a b = Var "%" :$ [a, b]
         return $ foldl1 mod' exprs
-    *<|> exprN (A8)
+    *<|> exprN A8
 
+-- match string addition (++) operator.
 exprN A6 =
     "append" ?: do
-        exprs <- sepBy1 (exprN $ A7) (try $ padString "++")
+        exprs <- sepBy1 (exprN A7) (try $ padString "++")
         return $ collector "++" exprs
-    *<|> exprN (A7)
+    *<|> exprN A7
 
+-- match sequences of addition and subtraction.
 exprN A5 =
     "addition/subtraction" ?: do
         -- Similar to multiply & divide
         -- eg. "1+2+3-4-5+6-7"
         --     [[1],[2],[3,4,5],[6,7]]
         exprs <- sepBy1
-            (sepBy1 (exprN $ A6) (try $ padString "-" ))
+            (sepBy1 (exprN A6) (try $ padString "-" ))
             (try $ padString "+" )
         let sub a b = Var "-" :$ [a, b]
-        return $ collector "+" $ map (foldl1 sub) exprs
-    *<|> exprN (A6)
+        return . collector "+" $ map (foldl1 sub) exprs
+    *<|> exprN A6
 
+-- match comparison operators.
 exprN A4 =
     do
-        firstExpr <- exprN $ A5
+        firstExpr <- exprN A5
         otherComparisonsExpr <- many $ do
             comparisonSymb <-
                      padString "=="
@@ -180,7 +226,7 @@ exprN A4 =
                 *<|> padString "<="
                 *<|> padString ">"
                 *<|> padString "<"
-            expr <- exprN $ A5
+            expr <- exprN A5
             return (Var comparisonSymb, expr)
         let
             (comparisons, otherExprs) = unzip otherComparisonsExpr
@@ -189,39 +235,43 @@ exprN A4 =
             []  -> firstExpr
             [x] -> x :$ exprs
             _   -> collector "all" $ zipWith3 (\c e1 e2 -> c :$ [e1,e2]) comparisons exprs (tail exprs)
-    *<|> exprN (A5)
+    *<|> exprN A5
 
+-- match the logical negation operator.
 exprN A3 =
     "logical-not" ?: do
         _ <- padString "!"
-        a <- exprN $ A4
+        a <- exprN A4
         return $ Var "!" :$ [a]
-    *<|> exprN (A4)
+    *<|> exprN A4
 
+-- match the logical And and Or (&&,||) operators.
 exprN A2 =
     "logical and/or" ?: do
-        a <- exprN $ A3
+        a <- exprN A3
         symb <-      padString "&&"
                 *<|> padString "||"
         b <- exprN A2
         return $ Var symb :$ [a,b]
-    *<|> exprN (A3)
+    *<|> exprN A3
 
+-- match the ternary (1?2:3) operator.
 exprN A1 =
     "ternary" ?: do
-        a <- exprN $ A2
+        a <- exprN A2
         _ <- padString "?"
         b <- exprN A1
         _ <- padString ":"
         c <- exprN A1
         return $ Var "?" :$ [a,b,c]
-    *<|> exprN (A2)
+    *<|> exprN A2
 
+-- Match and throw away any white space around an expression.
 exprN A0 =
     do
         _ <- genSpace
-        expr <- exprN $ A1
+        expr <- exprN A1
         _ <- genSpace
         return expr
-    *<|> exprN (A1)
+    *<|> exprN A1
 
