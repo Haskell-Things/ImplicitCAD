70fefd0ebbb0b8194cca8e039ac8d143904652f2
Julia Longtin <julialongtin@diasp.org>
2017-09-16 03:16:54 +0100

    import less, clear up some type definitions, and drop unneeded function arguments.

--- Graphics/Implicit/Export/PolylineFormats.hs
+++ Graphics/Implicit/Export/PolylineFormats.hs
@@ -9,9 +9,9 @@
 
 module Graphics.Implicit.Export.PolylineFormats where
 
-import Prelude((.), ($), (-), (+), (/), minimum, maximum, unzip, concat, show, (++), unwords, map, mapM_, snd, compare, min, max, Ord, Num)
+import Prelude((.), ($), (-), (+), (/), minimum, maximum, unzip, concat, show, (++), unwords, map, mapM_, snd, compare, min, max)
 
-import Graphics.Implicit.Definitions (Polyline, ℝ2)
+import Graphics.Implicit.Definitions (Polyline, ℝ, ℝ2)
 
 import Graphics.Implicit.Export.TextBuilderUtils (Text, Builder, mempty, toLazyText, mconcat, bf, (<>), buildTruncFloat)
 
@@ -20,21 +20,22 @@ import Text.Blaze.Svg11 ((!),docTypeSvg,g,polyline,toValue)
 import Text.Blaze.Internal (stringValue)
 import qualified Text.Blaze.Svg11.Attributes as A
 
-import qualified Data.List as List
+import Data.List (sortBy)
 
 svg :: [Polyline] -> Text
 svg plines = renderSvg . svg11 . svg' $ plines
     where
+      strokeWidth :: ℝ
       strokeWidth = 1.0
-      (xmin, xmax, ymin, ymax) = ((minimum xs) - margin, (maximum xs) + margin, (minimum ys) - margin, (maximum ys) + margin)
+      (xmin, xmax, ymin, ymax) = (minimum xs - margin, maximum xs + margin, minimum ys - margin, maximum ys + margin)
            where margin = strokeWidth / 2
                  (xs,ys) = unzip (concat plines)
       
-      svg11 content = docTypeSvg ! A.version "1.1" 
-                                 ! A.width  (stringValue $ show (xmax-xmin) ++ "mm")
-                                 ! A.height (stringValue $ show (ymax-ymin) ++ "mm")
-                                 ! A.viewbox (stringValue $ unwords . map show $ [0,0,xmax-xmin,ymax-ymin])
-                                 $ content
+      svg11 = docTypeSvg ! A.version "1.1"
+                         ! A.width  (stringValue $ show (xmax-xmin) ++ "mm")
+                         ! A.height (stringValue $ show (ymax-ymin) ++ "mm")
+                         ! A.viewbox (stringValue $ unwords . map show $ [0,0,xmax-xmin,ymax-ymin])
+
       -- The reason this isn't totally straightforwards is that svg has different coordinate system
       -- and we need to compute the requisite translation.
       svg' [] = mempty 
@@ -49,17 +50,18 @@ svg plines = renderSvg . svg11 . svg' $ plines
 
 hacklabLaserGCode :: [Polyline] -> Text
 hacklabLaserGCode polylines = toLazyText $ gcodeHeader <> mconcat (map interpretPolyline orderedPolylines) <> gcodeFooter
-    where 
+    where
+      orderedPolylines :: [Polyline]
       orderedPolylines =
-            snd . unzip 
-            . List.sortBy (\(a,_) (b, _) -> compare a b)
+            map snd
+            . sortBy (\(a,_) (b, _) -> compare a b)
             . map (\x -> (polylineRadius x, x))
             $ polylines
-      polylineRadius :: forall t. (Ord t, Num t) => [(t, t)] -> t
+      polylineRadius :: [(ℝ, ℝ)] -> ℝ
       polylineRadius [] = 0
       polylineRadius polyline' = max (xmax' - xmin') (ymax' - ymin') where
            ((xmin', xmax'), (ymin', ymax')) = polylineRadius' polyline'
-           polylineRadius' :: forall a a1. (Ord a1, Ord a, Num a1, Num a) => [(a, a1)] -> ((a, a), (a1, a1))
+           polylineRadius' :: [(ℝ, ℝ)] -> ((ℝ, ℝ), (ℝ, ℝ))
            polylineRadius' [] = ((0,0),(0,0))
            polylineRadius' [(x,y)] = ((x,x),(y,y))
            polylineRadius' ((x,y):ps) = ((min x xmin,max x xmax),(min y ymin, max y ymax))
@@ -79,7 +81,6 @@ hacklabLaserGCode polylines = toLazyText $ gcodeHeader <> mconcat (map interpret
                     ,"M2 (end)"]
       gcodeXY :: ℝ2 -> Builder
       gcodeXY (x,y) = mconcat ["X", buildTruncFloat x, " Y", buildTruncFloat y]
-                      
       interpretPolyline (start:others) = mconcat [
                                           "G00 ", gcodeXY start
                                          ,"\nM62 P0 (laser on)\n"
