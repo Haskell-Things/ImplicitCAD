5b7070e2fe84b014a1349873bb55558de64224c9
Julia Longtin <julialongtin@diasp.org>
2018-05-19 02:27:21 +0100

    comment the code, and be more explicit about imports.

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,8 +1,22 @@
-import Test.Hspec
-import ParserSpec.Statement
-import ParserSpec.Expr
+-- Implicit CAD. Copyright (C) 2011, Christopher Olah (chris@colah.ca)
+-- Copyright (C) 2018, Julia Longtin (julial@turinglace.com)
+-- Released under the GNU AGPLV3+, see LICENSE
+
+-- be explicit about what we import.
+import Prelude (($), IO)
+
+-- our testing engine.
+import Test.Hspec(hspec, describe)
+
+-- the test forstatements.
+import ParserSpec.Statement(statementSpec)
+
+-- the test for expressions.
+import ParserSpec.Expr(exprSpec)
 
 main :: IO ()
 main = hspec $ do
+  -- run tests against the expression engine.
   describe "expressions" exprSpec
+  -- and now, against the statement engine.
   describe "statements" statementSpec
