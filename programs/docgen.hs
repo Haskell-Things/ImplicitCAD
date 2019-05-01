39e0868dd5fb89f44984b2547637d81a7fa6989d
Julia Longtin <julia.longtin@gmail.com>
2019-05-01 14:29:38 +0100

    formatting changes, and minor formatting changes on the output.

--- programs/docgen.hs
+++ programs/docgen.hs
@@ -1,8 +1,14 @@
 -- Implicit CAD. Copyright (C) 2011, Christopher Olah (chris@colah.ca)
 -- Released under the GNU GPL, see LICENSE
 
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, FlexibleContexts, TypeSynonymInstances, UndecidableInstances, ScopedTypeVariables  #-}
-
+-- FIXME: document why we need each of these.
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 
 import Prelude(IO, Show, String, Int, Maybe(Just,Nothing), Eq, return, ($), show, fmap, (++), putStrLn, filter, zip, null, map, undefined, const, Bool(True,False), fst, snd, sequence, (.), concat, head, tail, sequence, length, (>), (/=), (+))
 import Graphics.Implicit.ExtOpenScad.Primitives (primitives)
@@ -11,77 +17,80 @@ import Graphics.Implicit.ExtOpenScad.Definitions (ArgParser(AP,APFailIf,APExampl
 import qualified Control.Exception as Ex (catch, SomeException)
 import Control.Monad (forM_, mapM)
 
+-- | Return true if the argument is of type ExampleDoc.
 isExample (ExampleDoc _ ) = True
 isExample _ = False
 
+-- | Return true if the argument is of type ArgumentDoc.
 isArgument (ArgumentDoc _ _ _) = True
 isArgument _ = False
 
+-- | Return true if the argument is of type Branch.
 isBranch (Branch _) = True
 isBranch _ = False
 
 dumpPrimitive :: String -> [DocPart] -> Int -> IO ()
 dumpPrimitive moduleName moduleDocList level = do
-            let
-                examples = filter isExample moduleDocList
-                arguments = filter isArgument moduleDocList
-                syntaxes = filter isBranch moduleDocList
-                moduleLabel = moduleName
+  let
+    examples = filter isExample moduleDocList
+    arguments = filter isArgument moduleDocList
+    syntaxes = filter isBranch moduleDocList
+    moduleLabel = moduleName
 
-            if level /= 0
-              then
-                do
-                  putStrLn $ "#" ++ moduleLabel
-              else
-                do
-                  putStrLn moduleLabel
-                  putStrLn (map (const '-') moduleLabel)
-            putStrLn ""
+  if level /= 0
+    then
+    do
+      putStrLn $ "#" ++ moduleLabel
+    else
+    do
+      putStrLn moduleLabel
+      putStrLn (map (const '-') moduleLabel)
+      putStrLn ""
 
-            if null examples
-              then
-                  return ()
-              else
-                  do
-                    putStrLn "#Examples:\n"
-                    forM_ examples $ \(ExampleDoc example) -> do
-                      putStrLn $ "   * `" ++ example ++ "`"
-                    putStrLn ""
+  if null examples
+    then
+    return ()
+    else
+    do
+      putStrLn "#Examples:\n"
+      forM_ examples $ \(ExampleDoc example) -> do
+        putStrLn $ "   * `" ++ example ++ "`"
+      putStrLn ""
 
-            if null arguments
-              then
-                  return ()
-              else
-                do
-                  if level /= 0
-                    then
-                      putStrLn "##Arguments:\n"
-                    else
-                      if null syntaxes
-                      then
-                          putStrLn "#Arguments:\n"
-                      else
-                          putStrLn "#Shared Arguments:\n"
-                  forM_ arguments $ \(ArgumentDoc name posfallback description) ->
-                      case (posfallback, description) of
-                        (Nothing, "") -> do
-                          putStrLn $ "   * `" ++ name  ++ "`"
-                        (Just fallback, "") -> do
-                          putStrLn $ "   * `" ++ name ++ " = " ++ fallback ++ "`"
-                        (Nothing, _) -> do
-                          putStrLn $ "   * `" ++ name ++ "`"
-                          putStrLn $ "     " ++ description
-                        (Just fallback, _) -> do
-                          putStrLn $ "   * `" ++ name ++ " = " ++ fallback ++ "`"
-                          putStrLn $ "     " ++ description
-                  putStrLn ""
+  if null arguments
+    then
+    return ()
+    else
+    do
+      if level /= 0
+        then
+        putStrLn "##Arguments:\n"
+        else
+          if null syntaxes
+          then
+            putStrLn "#Arguments:\n"
+          else
+            putStrLn "#Shared Arguments:\n"
+      forM_ arguments $ \(ArgumentDoc name posfallback description) ->
+        case (posfallback, description) of
+          (Nothing, "") -> do
+            putStrLn $ "   * `" ++ name  ++ "`"
+          (Just fallback, "") -> do
+            putStrLn $ "   * `" ++ name ++ " = " ++ fallback ++ "`"
+          (Nothing, _) -> do
+            putStrLn $ "   * `" ++ name ++ "`"
+            putStrLn $ "     " ++ description
+          (Just fallback, _) -> do
+            putStrLn $ "   * `" ++ name ++ " = " ++ fallback ++ "`"
+            putStrLn $ "     " ++ description
+      putStrLn ""
 
-            if null syntaxes
-              then
-                  return ()
-              else
-                  forM_ syntaxes $ \(Branch syntax) -> do
-                      dumpPrimitive ("Syntax " ++ (show $ level+1)) syntax (level+1)
+  if null syntaxes
+    then
+      return ()
+    else
+      forM_ syntaxes $ \(Branch syntax) -> do
+        dumpPrimitive ("Syntax " ++ (show $ level+1)) syntax (level+1)
 
 main :: IO ()
 main = do
@@ -98,16 +107,15 @@ main = do
         forM_ (zip names docs) $ \(moduleName, moduleDocList) -> do
           dumpPrimitive moduleName moduleDocList 0
 
--- | We need a format to extract documentation into
+-- | the format we extract documentation into
 data Doc = Doc String [DocPart]
              deriving (Show)
 
 data DocPart = ExampleDoc String
              | ArgumentDoc String (Maybe String) String
-             | Empty
              | Branch [DocPart]
-               deriving (Show,Eq)
-
+             | Empty
+               deriving (Show, Eq)
 
 --   Here there be dragons!
 --   Because we made this a Monad instead of applicative functor, there's no sane way to do this.
