b9ab29a5e74324d96c54a5e3a90a47c86ab0ae6f
Julia Longtin <julia.longtin@gmail.com>
2019-07-15 19:18:40 +0100

    whoops. actually upload the lexer.

---
+++ Graphics/Implicit/ExtOpenScad/Parser/Lexer.hs
@@ -0,0 +1,43 @@
+-- Implicit CAD. Copyright (C) 2011, Christopher Olah (chris@colah.ca)
+-- Copyright (C) 2016, Kelvin Cookshaw (kelvin@cookshaw.com)
+-- Copyright 2014 2015 2016, Julia Longtin (julial@turinglace.com)
+-- Released under the GNU AGPLV3+, see LICENSE
+
+module Graphics.Implicit.ExtOpenScad.Parser.Lexer (whiteSpace) where
+
+import Prelude (String, Char, Bool(True))
+
+import Control.Monad.Identity (Identity)
+
+import Text.Parsec.Token (GenTokenParser, makeTokenParser)
+
+import Text.Parsec.String (GenParser)
+
+import qualified Text.Parsec.Token as P (whiteSpace)
+
+import Text.Parsec.Language (GenLanguageDef, emptyDef)
+
+import Text.Parsec.Token (commentStart, commentEnd, commentLine, nestedComments, identStart, identLetter, reservedNames, reservedOpNames, caseSensitive)
+
+import Text.Parsec ((<|>), char, letter, alphaNum)
+
+openScadStyle :: GenLanguageDef String u0 Identity
+openScadStyle
+    = emptyDef
+    { commentStart = "/*"
+    , commentEnd = "*/"
+    , commentLine = "//"
+    , nestedComments = True
+    , identStart = letter <|> char '$' <|> char '_'
+    , identLetter = alphaNum <|> char '_'
+    , reservedNames = ["module", "function", "if", "else", "let", "for", "each", "true", "false", "undef", "include", "use"]
+    , reservedOpNames= ["<=", ">=", "==", "!=", "&&", "||"]
+    , caseSensitive = True
+    }
+
+lexer :: GenTokenParser String st Identity
+lexer = makeTokenParser openScadStyle
+
+-- Consume whitespace.
+whiteSpace :: GenParser Char st ()
+whiteSpace = P.whiteSpace lexer
