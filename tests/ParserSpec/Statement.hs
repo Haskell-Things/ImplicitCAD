2819af4c925dadacc5453f93312a526a6227c9c1
Julia Longtin <julia.longtin@gmail.com>
2019-08-07 18:07:58 +0100

    more test cases, and vary the spacing so that spacing is checked, as well.

--- tests/ParserSpec/Statement.hs
+++ tests/ParserSpec/Statement.hs
@@ -8,13 +8,13 @@
 -- | Statement related hspec tests.
 module ParserSpec.Statement (statementSpec) where
 
-import Prelude (String, Maybe(Just), Bool(True), ($))
+import Prelude (String, Maybe(Just, Nothing), Bool(True), ($))
 
 import Test.Hspec (Spec, Expectation, shouldBe, it, describe)
 
 import ParserSpec.Util (bool, num, minus, plus, mult, index)
 
-import Graphics.Implicit.ExtOpenScad.Definitions (StatementI(StatementI), Symbol(Symbol), Expr(ListE, LamE, (:$)), Statement(NewModule, ModuleCall, If, (:=)), Pattern(ListP), SourcePosition(SourcePosition))
+import Graphics.Implicit.ExtOpenScad.Definitions (StatementI(StatementI), Symbol(Symbol), Expr(ListE, LamE, (:$)), Statement(NewModule, ModuleCall, If, For, (:=)), Pattern(ListP), SourcePosition(SourcePosition))
 
 import qualified Graphics.Implicit.ExtOpenScad.Definitions as GIED (Expr(Var), Pattern(Name))
 
@@ -49,26 +49,20 @@ single st = [StatementI (SourcePosition 1 1 "noname") st]
 call :: String -> Fastℕ -> [(Maybe Symbol, Expr)] -> [StatementI] -> StatementI
 call name column args stmts = StatementI (SourcePosition 1 column "noname") (ModuleCall (Symbol name) args stmts)
 
--- | Test a simple if block.
-ifSpec :: Spec
-ifSpec = it "parses" $
-    "if (true) { a(); } else { b(); }" -->
-      single ( If (bool True) [call "a" 13 [] []] [call "b" 27 [] []])
-
 -- | Test assignments.
 assignmentSpec :: Spec
 assignmentSpec = do
   it "handles assignment" $
-    "y = -5;" --> single ( Name "y" := num (-5))
+    "y = -5 ; " --> single ( Name "y" := num (-5))
   it "handles pattern matching" $
-    "[x, y] = [1, 2];" --> single (ListP [Name "x", Name "y"] := ListE [num 1, num 2])
+    "[ x , y ] = [ 1 , 2 ] ; " --> single (ListP [Name "x", Name "y"] := ListE [num 1, num 2])
   it "handles the function keyword" $
-    "function foo(x, y) = x * y;" --> single fooFunction
+    "function foo ( x , y ) = x * y ; " --> single fooFunction
   it "handles function with let expression" $
-    "function withlet(b) = let (c = 5) b + c;" -->
+    "function withlet ( b ) = let ( c = 5 ) b + c ; " -->
     single (Name "withlet" := LamE [Name "b"] (LamE [Name "c"] (plus [Var "b", Var "c"]) :$ [num 5]))
   it "handles nested indexing" $
-    "x = [y[0] - z * 2];" -->
+    "x = [ y [ 0 ] - z * 2 ] ; " -->
     single ( Name "x" := ListE [minus [index [Var "y", num 0],
                                            mult [Var "z", num 2]]])
   where
@@ -76,38 +70,53 @@ assignmentSpec = do
     fooFunction = Name "foo" := LamE [Name "x", Name "y"]
                                 (mult [Var "x", Var "y"])
 
-{-
--- | the parser fails on as empty file. This can't be right.
-emptyFileIssue :: Expectation -> Expectation
-emptyFileIssue _ = pendingWith "parser should probably allow empty files"
--}
+-- | Test a simple if block.
+ifSpec :: Spec
+ifSpec = do
+  it "parses" $
+    "if ( true ) { a ( ) ; }" -->
+    single ( If (bool True) [call "a" 15 [] []] [])
+  it "parses with else clause" $
+    "if ( true ) { a ( ) ; } else {b();}" -->
+    single ( If (bool True) [call "a" 15 [] []] [call "b" 31 [] []])
 
--- | Our entry points. Test all of the statements.
+-- | Test a for loop.
+forSpec :: Spec
+forSpec = do
+  it "parses" $
+    "for (i = [ 1 : 10 ] ) { } " --> single (For (Name "i") (Var "list_gen" :$ [ListE [num 1, num 1, num 10]]) [])
+
+-- | Our entry point. Test all of the statements.
 statementSpec :: Spec
 statementSpec = do
   describe "empty file" $
     it "returns an empty list" $ "" --> []
   describe "assignment" assignmentSpec
   describe "if" ifSpec
+  describe "for" forSpec
   describe "line comment" $
-    it "parses as empty" $ "// foish bar\n" --> []
+    it "parses as empty" $ "// foish bar \n " --> []
   describe "multiline comment" $
-    it "parses as empty" $ "/* foish bar\n*/" --> []
+    it "parses as empty" $ "/* foish bar\n */ " --> []
   describe "module call" $
-    it "parses" $ "foo();" --> single (ModuleCall (Symbol "foo") [] [])
+    it "parses" $ "foo ( ) ; " --> single (ModuleCall (Symbol "foo") [] [])
   describe "disabled module call" $
-    it "parses as empty" $ "% foo ();" --> []
+    it "parses as empty" $ "% foo ( ) ; " --> []
   describe "difference of two cylinders" $
     it "parses correctly" $
-      "difference(){ cylinder(r=5,h=20); cylinder(r=2,h=20); }"
+      "difference ( ) { cylinder ( r = 5 , h = 20 ) ;cylinder(r=2,h=20); } "
       --> single (
         ModuleCall (Symbol "difference") [] [
-           call "cylinder" 15 [(Just (Symbol "r"), num 5.0),
+           call "cylinder" 18 [(Just (Symbol "r"), num 5.0),
                              (Just (Symbol "h"), num 20.0)]
             [],
-           call "cylinder" 35 [(Just (Symbol "r"), num 2.0),
+           call "cylinder" 47 [(Just (Symbol "r"), num 2.0),
                              (Just (Symbol "h"), num 20.0)]
             []])
-  describe "empty module definition" $
+  describe "module definition" $ do
     it "parses correctly" $
-      "module foo_bar() {}" --> single (NewModule (Symbol "foo_bar") [] [])
+      "module foo_bar ( ) { }" --> single (NewModule (Symbol "foo_bar") [] [])
+    it "accepts argument" $
+      "module foo_bar ( x ) { }" --> single (NewModule (Symbol "foo_bar") [(Symbol "x", Nothing)] [])
+    it "accepts argument with default" $
+      "module foo_bar ( x = 1) { }" --> single (NewModule (Symbol "foo_bar") [(Symbol "x", Just $ num 1)] [])
