4b98e41eb2878b2cbb32247bc380e35df4e6b080
Julia Longtin <julia.longtin@gmail.com>
2019-10-27 22:08:18 +0000

    use deriving rules, instead of deriving manually. thanks, tilt!

--- Graphics/Implicit/IntegralUtil.hs
+++ Graphics/Implicit/IntegralUtil.hs
@@ -2,13 +2,14 @@
 -- Copyright 2014-2019, Julia Longtin (julial@turinglace.com)
 -- Released under the GNU AGPLV3+, see LICENSE
 
+-- Lift the numeric instances where we can
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- Suppress a warning about the derived Integral instance
+{-# OPTIONS_GHC -Wno-identities #-}
+
 module Graphics.Implicit.IntegralUtil (ℕ, toℕ, fromℕ) where
 
-import Prelude (Integral(toInteger, quot, rem, quotRem, div, mod, divMod), Num((+), (*), abs, negate, signum, fromInteger), Eq, Ord, Enum(succ, pred, toEnum, fromEnum), Real(toRational), Show, ($), Read, fromIntegral, Int, Integer, (.))
-
-import qualified Prelude as P ((+), (*), abs, negate, signum, succ, pred, toEnum, fromEnum, quotRem, divMod)
-
-import GHC.Real (Ratio((:%)))
+import Prelude (Integral, Integer, Int, Show, Read, Eq, Ord, Num, Enum, Integral, Real, ($), fromIntegral, (.))
 
 -- So we can produce an instance of Fastℕ for ℕ.
 import Graphics.Implicit.FastIntUtil (Fastℕ(Fastℕ))
@@ -37,58 +38,7 @@ instance N Int where
   {-# INLINABLE toℕ #-}
 
 -- Arbitrary precision integers. To be used for anything countable, or in ratios.
+-- When Read and Show instances exist on a given type they need to satisfy
+-- read . show = id
 newtype ℕ = ℕ Integer
-  deriving (Show, Read, Eq, Ord)
-
-instance Real ℕ where
-  toRational (ℕ a) = a :% 1
-  {-# INLINABLE toRational #-}
-
-bothℕ :: (Integer, Integer) -> (ℕ, ℕ)
-bothℕ (a, b) = (ℕ a , ℕ b)
-
-instance Integral ℕ where
-  toInteger (ℕ a)     = a
-  {-# INLINABLE toInteger #-}
-  quot (ℕ n) (ℕ d)    = ℕ q where (q,_) = quotRem n d
-  {-# INLINABLE quot #-}
-  rem (ℕ n) (ℕ d)     = ℕ r where (_,r) = quotRem n d
-  {-# INLINABLE rem #-}
-  quotRem (ℕ a) (ℕ b) = bothℕ $ P.quotRem a b 
-  {-# INLINABLE quotRem #-}
-  div (ℕ n) (ℕ d)     = ℕ q where (q,_) = divMod n d
-  {-# INLINABLE div #-}
-  mod (ℕ n) (ℕ d)     = ℕ r where (_,r) = divMod n d
-  {-# INLINABLE mod #-}
-  divMod (ℕ n) (ℕ d)  = bothℕ $ P.divMod n d
-  {-# INLINABLE divMod #-}
-
-instance Num ℕ where
-  (+) (ℕ a) (ℕ b) = ℕ $ a P.+ b
-  {-# INLINABLE (+) #-}
-  (*) (ℕ a) (ℕ b) = ℕ $ a P.* b
-  {-# INLINABLE (*) #-}
-  abs (ℕ a)       = ℕ $ P.abs a
-  {-# INLINABLE abs #-}
-  negate (ℕ a)    = ℕ $ P.negate a
-  {-# INLINABLE negate #-}
-  signum (ℕ a)    = ℕ $ P.signum a
-  {-# INLINABLE signum #-}
-  fromInteger     = ℕ
-  {-# INLINABLE fromInteger #-}
-
--- | Note that we do not implement all of the members of the typeclass here.
-instance Enum ℕ where
-  succ (ℕ x) = ℕ $ P.succ x
-  {-# INLINABLE succ #-}
-  pred (ℕ x) = ℕ $ P.pred x
-  {-# INLINABLE pred #-}
-  toEnum n = ℕ $ P.toEnum n
-  {-# INLINABLE toEnum #-}
-  fromEnum (ℕ n) = P.fromEnum n
-  {-# INLINABLE fromEnum #-}
-  
-
-
-
-
+ deriving (Show, Read, Eq, Ord, Num, Enum, Integral, Real)
