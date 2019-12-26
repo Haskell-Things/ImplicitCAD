bb3893fdfeb689e0017aca7b7745c8799a4f8af2
Julia Longtin <julia.longtin@gmail.com>
2019-12-26 13:03:55 +0000

    fmap instead of map.

--- Examples/example17.hs
+++ Examples/example17.hs
@@ -10,12 +10,12 @@ object2 = squarePipe (10,10,10) 1 100
       squarePipe :: (ℝ,ℝ,ℝ) -> ℝ -> ℝ -> SymbolicObj3
       squarePipe (x,y,z) diameter precision =
             union
-            $ map (\start-> translate start
+            $ fmap (\start-> translate start
                    $ rect3R 0 (0,0,0) (diameter,diameter,diameter)
                   )
-            $ zip3 (map (\n->(fromIntegral n/precision)*x) [0..100])
-                   (map (\n->(fromIntegral n/precision)*y) [0..100])
-                   (map (\n->(fromIntegral n/precision)*z) [0..100])
+            $ zip3 (fmap (\n->(fromIntegral n/precision)*x) [0..100])
+                   (fmap (\n->(fromIntegral n/precision)*y) [0..100])
+                   (fmap (\n->(fromIntegral n/precision)*z) [0..100])
 
 main = writeSTL 1 "example17.stl" object2
 
