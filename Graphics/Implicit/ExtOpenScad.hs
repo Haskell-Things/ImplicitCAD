3f3d4d50fa6a525103bae27757a0c065e427185b
Julia Longtin <julia.longtin@gmail.com>
2020-01-12 18:56:09 +0000

    remove unneeded language extensions.

--- Graphics/Implicit/ExtOpenScad.hs
+++ Graphics/Implicit/ExtOpenScad.hs
@@ -2,9 +2,6 @@
 -- Copyright (C) 2014 2015 2016, Julia Longtin (julial@turinglace.com)
 -- Released under the GNU AGPLV3+, see LICENSE
 
--- FIXME: why are these required?
-{-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
-
 -- allow us to specify what package to import what module from.
 -- We don't actually care, but when we compile our haskell examples, we do.
 {-# LANGUAGE PackageImports #-}
