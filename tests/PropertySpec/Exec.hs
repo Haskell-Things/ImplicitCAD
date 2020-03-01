34130d96c044e804bfd4b3d045380f38c238d4b8
Owen Harvey <owenlharvey@gmail.com>
2020-03-01 13:05:51 +1000

    Folding functions and names to be based on a single sum type.

--- tests/PropertySpec/Exec.hs
+++ tests/PropertySpec/Exec.hs
@@ -14,8 +14,8 @@ import           HaskellWorks.Hspec.Hedgehog                 (requireProperty)
 import           Hedgehog                                    (diff, forAll)
 import qualified Hedgehog.Gen                                as Gen
 import qualified Hedgehog.Range                              as Range
-import           Prelude                                     (Bool, Floating, String, Double,
-                                                              Ord, fail, show,
+import           Prelude                                     (Bool (False), Floating, String, Double, Show,
+                                                              Eq, Ord, fail, show,
                                                               ($), (&&), (+),
                                                               (.), (<$>), (<=), (*), (/),
                                                               (<>), (>=), (-))
@@ -24,24 +24,46 @@ import           Test.Hspec                                  (Spec, it)
 approx :: (Floating a, Ord a) => a -> a -> a -> Bool
 approx z a b = a + z >= b && a <= b + z
 
-mathsSpec :: (Double -> Double -> Double) -> String -> Spec
-mathsSpec f s =
-  it "addition" . requireProperty $ do
+data Op = Add | Sub | Mul | Div
+  deriving Eq
+
+instance Show Op where
+  show Add = "+"
+  show Sub = "-"
+  show Mul = "*"
+  show Div = "/"
+
+opName :: Op -> String 
+opName Add = "addition"
+opName Sub = "subttraction"
+opName Mul = "multiplication"
+opName Div = "division"
+
+fromOp :: Op -> Double -> Double -> Double
+fromOp Add = (+)
+fromOp Sub = (-)
+fromOp Mul = (*)
+fromOp Div = (/)
+
+mathsSpec :: Op -> Spec
+mathsSpec o =
+  it (opName o) . requireProperty $ do
+    -- up to 100 values, between 1 and 1000
     l <- forAll . Gen.nonEmpty (Range.linear 1 100) . Gen.double $ Range.linearFrac 1 1000
-    let e = fold . intersperse s $ show <$> l
-        n = foldl1 f l
-    case (runExpr e, num n) of
+    let e = fold . intersperse (show o) $ show <$> l
+        n = foldl1 (fromOp o) l
+    case (runExpr e False, num n) of
       ((ONum a, []), ONum b) -> diff a (approx 0.000001) b -- Some value to supress floating point inaccuracies
       (a, _)                 -> fail $ "Unexpected result value " <> show a
 
 additionSpec :: Spec
-additionSpec = mathsSpec (+) "+"
+additionSpec = mathsSpec Add
 
 subtractionSpec :: Spec
-subtractionSpec = mathsSpec (-) "-"
+subtractionSpec = mathsSpec Sub
 
 multiplicationSpec :: Spec
-multiplicationSpec = mathsSpec (*) "*"
+multiplicationSpec = mathsSpec Mul
 
 divisionSpec :: Spec
-divisionSpec = mathsSpec (/) "/"
+divisionSpec = mathsSpec Div
