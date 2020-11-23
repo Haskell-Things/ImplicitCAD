6da09b1591e7d0a348ffa67ac131c28df1b21f9b
Sandy Maguire <sandy@sandymaguire.me>
2020-11-23 09:35:55 -0800

    Fix comment that was accidentally scrambled by tooling

--- Graphics/Implicit/MathUtil.hs
+++ Graphics/Implicit/MathUtil.hs
@@ -1,5 +1,5 @@
--- Copyright (C) 2014 2015 2016, Julia Longtin (julial@turinglace.com)
 -- Implicit CAD. Copyright (C) 2011, Christopher Olah (chris@colah.ca)
+-- Copyright (C) 2014 2015 2016, Julia Longtin (julial@turinglace.com)
 -- Released under the GNU AGPLV3+, see LICENSE
 
 {-# LANGUAGE FlexibleContexts #-}
