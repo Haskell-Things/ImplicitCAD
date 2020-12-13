759176d9c7a02db2fdf479d14ccb4369c3f5b8aa
Sandy Maguire <sandy@sandymaguire.me>
2020-12-13 12:59:38 -0800

    Add "loops a loop" test and haddock

--- tests/TesselationSpec.hs
+++ tests/TesselationSpec.hs
@@ -34,6 +34,12 @@ spec = describe "getLoops" $ do
       for_ (zip loops shuffled_segs) $ \(loop, seg) ->
         head loop `shouldBe` head seg
 
+  prop "loops a loop" $ do
+    (v, segs) <- genLoop @Int 0
+    pure $ do
+      Just [loop] <- pure $ getLoops segs
+      proveLoop v loop
+
   prop "loops many loops" $ do
     -- Pick a number of loops to aim for
     n <- choose (2, 20)
@@ -49,22 +55,40 @@ spec = describe "getLoops" $ do
       for_ (zip vs $ sort loops) $ uncurry proveLoop
 
 
+------------------------------------------------------------------------------
+-- | Show that the given loop exists somewhere in the discovered loops.
+-- Correctly deals with the case where the two loops start at different places.
 proveLoop :: (Show a, Eq a) => [a] -> [[a]] -> Expectation
 proveLoop v loops =
-  join (replicate 2 v) `shouldContain` unsegment loops
+  join (replicate 2 v) `shouldContain` unloop loops
 
 
-genLoop :: Enum a => a -> Gen ([a], [[a]])
+------------------------------------------------------------------------------
+-- | Generate a loop and random segments that should produce it. The defining
+-- equation of this generator is tested by "getLoops > loops a loop".
+genLoop
+    :: Enum a
+    => a
+    -> Gen ([a], [[a]])  -- ^ @(loop, segments)@
 genLoop start = do
   n <- getPositive <$> arbitrary @(Positive Int)
   let v = take n $ enumFrom start
   bits <- randomGroups v
-  let segs = mkSegments bits
+  let segs = loopify bits
   shuffled_segs <- shuffle segs
   pure (v, shuffled_segs)
 
 
-genManyLoops :: Enum a => a -> Int -> Gen ([[(Int, a)]], [[[(Int, a)]]])
+------------------------------------------------------------------------------
+-- | Like 'genLoop', but produces several loops, tagged with an index number.
+-- For best results, you should call @shuffle . join@ on the resulting segments
+-- before calling @getLoops@ on it, to ensure the segments are intermingled
+-- between the loops.
+genManyLoops
+    :: Enum a
+    => a
+    -> Int  -- ^ Number of loops to generate
+    -> Gen ([[(Int, a)]], [[[(Int, a)]]])  -- ^ @(loop, segments)@
 genManyLoops start n = do
   fmap unzip $ for [0 .. n - 1] $ \idx -> do
     -- Generate a loop for each
@@ -73,10 +97,15 @@ genManyLoops start n = do
     pure (fmap (idx,) v, fmap (fmap (idx,)) segs)
 
 
+------------------------------------------------------------------------------
+-- | Given a list of lists, insert elements into the 'head' and 'last' of each
+-- sub-list so that the 'last' of one list is the 'head' of the next.
+loopify :: [[a]] -> [[a]]
+loopify as = zipWith (\a -> mappend a . take 1) as $ drop 1 $ join $ repeat as
 
-mkSegments :: [[a]] -> [[a]]
-mkSegments as = zipWith (\a -> mappend a . take 1) as $ drop 1 $ join $ repeat as
-
-unsegment :: Eq a => [[a]] -> [a]
-unsegment = drop 1 . fmap head . group . join
+------------------------------------------------------------------------------
+-- | Remove sequential elements in a list. Additionally, this function removes
+-- the 'head' of the list, because conceptully it is also the 'last'.
+unloop :: Eq a => [[a]] -> [a]
+unloop = drop 1 . fmap head . group . join
 
