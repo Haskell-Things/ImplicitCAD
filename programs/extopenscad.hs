5b68b061b97a185e8458b7810adb1f4b27e631ef
Matthias Fischmann <mf@zerobuzz.net>
2021-03-16 19:46:53 +0100

    Have comments inside the right-side of a case.
    This makes it less ambiguous where the comment belongs, both to
    machine and human readers.

--- programs/extopenscad.hs
+++ programs/extopenscad.hs
@@ -192,28 +192,30 @@ instance Read OutputFormat where
 
 -- | Find the resolution to raytrace at.
 getRes :: (VarLookup, [SymbolicObj2], [SymbolicObj3], [Message]) -> ℝ
--- | If specified, use a resolution specified by the "$res" a variable in the input file.
-getRes (lookupVarIn "$res" -> Just (ONum res), _, _, _) = res
--- | If there was no resolution specified, use a resolution chosen for 3D objects.
---   FIXME: magic numbers.
+getRes (lookupVarIn "$res" -> Just (ONum res), _, _, _) =
+    -- If specified, use a resolution specified by the "$res" a variable in the input file.
+    res
 getRes (vars, _, obj:objs, _) =
+    -- If there was no resolution specified, use a resolution chosen for 3D objects.
+    -- FIXME: magic numbers.
     let
         (V3 x1 y1 z1, V3 x2 y2 z2) = getBox (unionR 0 (obj:objs))
         (V3 x y z) = V3 (x2-x1) (y2-y1) (z2-z1)
     in case fromMaybe (ONum 1) $ lookupVarIn "$quality" vars of
         ONum qual | qual > 0  -> min (minimum [x,y,z]/2) ((x*y*z/qual)**(1/3) / 22)
         _                     -> min (minimum [x,y,z]/2) ((x*y*z)**(1/3) / 22)
--- | ... Or use a resolution chosen for 2D objects.
---   FIXME: magic numbers.
 getRes (vars, obj:objs, _, _) =
+    -- ... Or use a resolution chosen for 2D objects.
+    --   FIXME: magic numbers.
     let
         (p1,p2) = getBox (unionR 0 (obj:objs))
         (V2 x y) = p2 .-. p1
     in case fromMaybe (ONum 1) $ lookupVarIn "$quality" vars of
         ONum qual | qual > 0 -> min (min x y/2) (sqrt(x*y/qual) / 30)
         _                    -> min (min x y/2) (sqrt(x*y) / 30)
--- | fallthrough value.
-getRes _ = 1
+getRes _ =
+    -- fallthrough value.
+    1
 
 -- | Output a file containing a 3D object.
 export3 :: Maybe OutputFormat -> ℝ -> FilePath -> SymbolicObj3 -> IO ()
