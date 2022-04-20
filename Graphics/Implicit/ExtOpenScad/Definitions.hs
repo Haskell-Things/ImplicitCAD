546a74be9abff65743160b801e165b10f6139a1d
Richard Marko <srk@48.io>
2022-04-20 13:27:46 +0200

    add Default ScadOpts

--- Graphics/Implicit/ExtOpenScad/Definitions.hs
+++ Graphics/Implicit/ExtOpenScad/Definitions.hs
@@ -27,7 +27,7 @@ module Graphics.Implicit.ExtOpenScad.Definitions (ArgParser(AP, APTest, APBranch
                                                   varUnion
                                                   ) where
 
-import Prelude(Eq, Show, Ord, Maybe(Just), Bool(False), IO, FilePath, (==), show, ($), (<>), and, zipWith, Int, (<$>))
+import Prelude(Eq, Show, Ord, Maybe(Just), Bool(True, False), IO, FilePath, (==), show, ($), (<>), and, zipWith, Int, (<$>))
 
 -- Resolution of the world, Integer type, and symbolic languages for 2D and 3D objects.
 import Graphics.Implicit.Definitions (ℝ, ℕ, Fastℕ, SymbolicObj2, SymbolicObj3, fromFastℕ)
@@ -35,6 +35,8 @@ import Graphics.Implicit.Definitions (ℝ, ℕ, Fastℕ, SymbolicObj2, SymbolicO
 import Control.Applicative (Applicative, Alternative((<|>), empty), pure, (<*>))
 
 import Control.Monad (Functor, Monad, (>>=), mzero, mplus, MonadPlus, ap, (>=>))
+
+import Data.Default.Class (Default(def))
 
 import Data.Map (Map, lookup, union)
 
@@ -219,6 +221,12 @@ data ScadOpts = ScadOpts
   { openScadCompatibility :: Bool
   , importsAllowed        :: Bool
   }
+
+instance Default ScadOpts where
+  def = ScadOpts
+    { openScadCompatibility = False
+    , importsAllowed        = True
+    }
 
 -- helper, to use union on VarLookups.
 varUnion :: VarLookup -> VarLookup -> VarLookup
